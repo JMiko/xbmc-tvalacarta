f45fadcbb1fd6b162da36a22ab123b7e5e691340
ermanitu <ermanitu@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2010-01-06 17:55:12 +0000

    Prueba de ermanitu para probar el svn

--- branches/plugins/video/pelisalacarta/channels/descargacineclasico.py
+++ branches/plugins/video/pelisalacarta/channels/descargacineclasico.py
@@ -3,6 +3,7 @@
 # pelisalacarta - XBMC Plugin
 # Canal para descargacineclasico
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+# contribución de ErManitu
 #------------------------------------------------------------
 import urlparse,urllib2,urllib,re
 import os
