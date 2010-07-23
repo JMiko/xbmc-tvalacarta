fee876cbaf4586c9a56803e93a24769882e68e40
jurrabi@gmail.com <jurrabi@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2010-07-23 12:37:58 +0000

    Mejora config.py. Sin ser ideal aún, pero he mejorado un poco la gestión del try except en el config para evitar enmascarar otros posibles errores en la detección de la versión de XBMC.

--- branches/plugins/video/pelisalacarta/resources/lib/config.py
+++ branches/plugins/video/pelisalacarta/resources/lib/config.py
@@ -8,48 +8,49 @@
 import xbmc
 import xbmcplugin
 import sys
+try:
+	import xbmcaddon
+	DHARMA = True
+except ImportError:
+	DHARMA = False
 
 def openSettings():
 	
 	# Nuevo XBMC
-	try:
-		import xbmcaddon
+	if DHARMA:
 		__settings__ = xbmcaddon.Addon(id="plugin.video.pelisalacarta")
 		__settings__.openSettings()
 	# Antiguo XBMC
-	except ImportError:
+	else:
 		xbmcplugin.openSettings( sys.argv[ 0 ] )
 
 def getSetting(name):
 	# Nuevo XBMC
-	try:
-		import xbmcaddon
+	if DHARMA:
 		__settings__ = xbmcaddon.Addon(id="plugin.video.pelisalacarta")
 		return __settings__.getSetting( name )
 	# Antiguo XBMC
-	except ImportError:
+	else:
 		value = xbmcplugin.getSetting(name)
 		xbmc.output("[config.py] antiguo getSetting(%s)=%s" % (name,value))
 		return value
 
 def setSetting(name,value):
 	# Nuevo XBMC
-	try:
-		import xbmcaddon
+	if DHARMA:
 		__settings__ = xbmcaddon.Addon(id="plugin.video.pelisalacarta")
 		__settings__.setSetting( name,value ) # this will return "foo" setting value
 	# Antiguo XBMC
-	except ImportError:
+	else:
 		xbmcplugin.setSetting("name",value)
 
 def getLocalizedString(code):
 	# Nuevo XBMC
-	try:
-		import xbmcaddon
+	if DHARMA:
 		__settings__ = xbmcaddon.Addon(id="plugin.video.pelisalacarta")
 		__language__ = __settings__.getLocalizedString
 		__language__ = __settings__.getLocalizedString
 		return __language__(code)
 	# Antiguo XBMC
-	except ImportError:
+	else:
 		return "no implementado"
