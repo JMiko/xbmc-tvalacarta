b5beb7dac778c3dddd8a24b039be1e2d70e3f687
jurrabi@gmail.com <jurrabi@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2010-07-30 18:46:47 +0000

    Modificación para permitir remote-debugging desde Eclipse (no afecta al resto)

--- plugins/video/plugin.video.pelisalacarta/default.py
+++ plugins/video/plugin.video.pelisalacarta/default.py
@@ -18,6 +18,7 @@ import xbmc
 
 xbmc.output("[default.py] pelisalacarta init...")
 
+
 # Configura los directorios donde hay librerías
 librerias = xbmc.translatePath( os.path.join( os.getcwd(), 'resources', 'lib' ) )
 sys.path.append (librerias)
@@ -28,6 +29,28 @@ sys.path.append (librerias)
 librerias = xbmc.translatePath( os.path.join( os.getcwd(), 'youtubeAPI' ) )
 sys.path.append (librerias)
 
+import logger
+from logger import REMOTE_DBG
+
+# append pydev remote debugger
+if REMOTE_DBG:
+    # Make pydev debugger works for auto reload.
+    # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
+    try:
+        import pysrc.pydevd as pydevd
+    # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+    # El server está en el PC donde tengas ejecutando eclipse
+        pydevd.settrace('192.168.1.30', stdoutToServer=True, stderrToServer=True)
+        logger.debug ("[default.py] REMOTE_DGB ON -  conectando con el server en 192.168.1.30")
+    except ImportError:
+        xbmc.output("[default.py] REMOTE_DBG Error: " +
+            "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
+        REMOTE_DBG = False 
+
+
+
 # Ejecuta el programa principal
 import pelisalacarta
 pelisalacarta.run()
+
+
