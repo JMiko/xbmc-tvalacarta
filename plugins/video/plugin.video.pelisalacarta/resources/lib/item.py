e20abb6d5b754a7302219d7568d92135d02921f6
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2010-11-30 22:03:34 +0000

    Generalizacion de canales

--- plugins/video/plugin.video.pelisalacarta/resources/lib/item.py
+++ plugins/video/plugin.video.pelisalacarta/resources/lib/item.py
@@ -1,28 +1,32 @@
 class Item(object):
-	channel = ""
-	title = ""
-	url = ""
-	thumbnail = ""
-	plot = ""
-	duration = ""
-	fanart = ""
-	folder = ""
-	action = ""
-	server = ""
-	extra = ""
+    channel = ""
+    title = ""
+    url = ""
+    thumbnail = ""
+    plot = ""
+    duration = ""
+    fanart = ""
+    folder = True
+    action = ""
+    server = ""
+    extra = ""
+    category = ""
+    show = ""
 
-	def __init__(self, channel="", title="", url="", thumbnail="", plot="", duration="", fanart="", action="", server="", extra="", folder=False):
-		self.channel = channel
-		self.title = title
-		self.url = url
-		self.thumbnail = thumbnail
-		self.plot = plot
-		self.duration = duration
-		self.fanart = fanart
-		self.folder = folder
-		self.server = server
-		self.action = action
-		self.extra = extra
+    def __init__(self, channel="", title="", url="", thumbnail="", plot="", duration="", fanart="", action="", server="", extra="", category = "", show = "" , folder=True):
+        self.channel = channel
+        self.title = title
+        self.url = url
+        self.thumbnail = thumbnail
+        self.plot = plot
+        self.duration = duration
+        self.fanart = fanart
+        self.folder = folder
+        self.server = server
+        self.action = action
+        self.extra = extra
+        self.category = category
+        self.show = show
 
-	def tostring(self):
-		return "title=["+self.title+"], url=["+self.url+"], thumbnail=["+self.thumbnail+"]"
+    def tostring(self):
+        return "title=["+self.title+"], url=["+self.url+"], thumbnail=["+self.thumbnail+"]"
