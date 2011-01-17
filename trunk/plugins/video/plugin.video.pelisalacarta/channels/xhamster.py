1553dcd3af37d2277325e5cc4055c9d74ffcdcad
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-01-17 12:15:44 +0000

    Arreglado un problema con la expresión regular de la paginación (@zodreis). Convertido a canal genérico.

--- trunk/plugins/video/plugin.video.pelisalacarta/channels/xhamster.py
+++ trunk/plugins/video/plugin.video.pelisalacarta/channels/xhamster.py
@@ -5,75 +5,46 @@
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 #------------------------------------------------------------
 import urlparse,urllib2,urllib,re
-import os
-import sys
-import xbmc
-import xbmcgui
-import xbmcplugin
+import os, sys
+
 import scrapertools
-import megavideo
 import servertools
-import binascii
-import xbmctools
-import config
-from item import Item
 import logger
+from item import Item
 
 CHANNELNAME = "xhamster"
-
-# Esto permite su ejecuci�n en modo emulado
-try:
-    pluginhandle = int( sys.argv[ 1 ] )
-except:
-    pluginhandle = ""
-
-# Traza el inicio del canal
-logger.info("[xhamster.py] init")
-
 DEBUG = True
 
-def mainlist(params,url,category):
-    logger.info("[xhamster.py] mainlist")
-    
-    itemlist = getmainlist(params,url,category)
-    xbmctools.renderItems(itemlist, params, url, category)
+def isGeneric():
+    return True
 
-def getmainlist(params,url,category):
-    logger.info("[xhamster.py] getmainlist")
+def mainlist(item):
+    logger.info("[yotix.py] mainlist")
 
     itemlist = []
-    itemlist.append( Item( channel=CHANNELNAME , title="Novedades" , action="novedades" , url="http://www.xhamster.com/" , folder=True ) )
-    
+    itemlist.append( Item(channel=CHANNELNAME, action="novedades" , title="Novedades", url="http://www.xhamster.com/"))
+
     return itemlist
 
-def novedades(params,url,category):
+def novedades(item):
     logger.info("[xhamster.py] novedades")
 
-    itemlist = getnovedades(params,url,category)
-    xbmctools.renderItems(itemlist, params, url, category)
-
-def getnovedades(params,url,category):
-    logger.info("[xhamster.py] getnovedades")
-
-    # ------------------------------------------------------
     # Descarga la página
-    # ------------------------------------------------------
-    data = scrapertools.cachePage(url)
+    data = scrapertools.cachePage(item.url)
     #logger.info(data)
 
-    # ------------------------------------------------------
     # Extrae las entradas
-    # ------------------------------------------------------
     # seccion novedades
     '''
-<a href="/movies/496069/the_cheerleader.html" class="hRotator">
-<img src="http://st5.xhamster.com/t/069/2_496069.jpg" height="120" width="160">
-<img class="hSprite" src="http://static.xhamster.com/images/spacer.gif" sprite="http://st5.xhamster.com/t/069/s_496069.jpg" id="496069" onmouseover="hRotator.start2(this);" height="120" width="160">
-</a>
-      <div class="moduleFeaturedTitle">
-        <a href="/movies/496069/the_cheerleader.html">The Cheerleader</a>
-      </div>
-      <div class="moduleFeaturedDetails">Runtime: 35m51s<br><span style="color: Green;">'''
+    <a href="/movies/496069/the_cheerleader.html" class="hRotator">
+    <img src="http://st5.xhamster.com/t/069/2_496069.jpg" height="120" width="160">
+    <img class="hSprite" src="http://static.xhamster.com/images/spacer.gif" sprite="http://st5.xhamster.com/t/069/s_496069.jpg" id="496069" onmouseover="hRotator.start2(this);" height="120" width="160">
+    </a>
+    <div class="moduleFeaturedTitle">
+    <a href="/movies/496069/the_cheerleader.html">The Cheerleader</a>
+    </div>
+    <div class="moduleFeaturedDetails">Runtime: 35m51s<br><span style="color: Green;">
+    '''
 
     #patronvideos  = '<p style="text-align: center;">.*?'
     patronvideos = '<a href="(/movies/[^"]+.html)"[^>]*?>[^<]*?'
@@ -84,7 +55,6 @@ def getnovedades(params,url,category):
     patronvideos += '</div[^<]*?>[^<]*?'
     patronvideos += '<div[^<]*?>Runtime: ([^<]+)<'
 
-
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     if DEBUG: scrapertools.printMatches(matches)
 
@@ -92,44 +62,41 @@ def getnovedades(params,url,category):
     for match in matches:
         # Titulo
         scrapedtitle = match[2] + " [" + match[3] + "]"
-        scrapedurl = urlparse.urljoin(url,match[0])
+        scrapedurl = urlparse.urljoin(item.url,match[0])
         scrapedthumbnail = match[1].replace(" ", "%20")
         scrapedplot = scrapertools.htmlclean(match[2].strip())
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
 
         # Añade al listado de XBMC
-        itemlist.append( Item(channel=CHANNELNAME, action="detail", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=False) )
+        itemlist.append( Item(channel=CHANNELNAME, action="findvideos", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
 
-    # ------------------------------------------------------
     # Extrae el paginador
-    # ------------------------------------------------------
     #<A HREF="/new/2.html">Next</A><
-    patronvideos  = '<a href="\/(new\/[^\.]+\.html)"[^>]*?>Next[^<]*?<\/a>'
+    patronvideos  = '<a href="(\/new\/[^\.]+\.html)"[^>]*?>Next[^<]*?<\/a>'
     matches = re.compile(patronvideos,re.DOTALL | re.IGNORECASE).findall(data)
     scrapertools.printMatches(matches)
 
     if len(matches)>0:
-        scrapedurl = urlparse.urljoin(url,matches[0])
+        scrapedurl = urlparse.urljoin(item.url,matches[0])
         logger.info("[xhamster.py] " + scrapedurl)
         itemlist.append( Item(channel=CHANNELNAME, action="novedades", title="!Página siguiente" , url=scrapedurl , folder=True) )
 
     return itemlist
 
-def detail(params,url,category):
-    logger.info("[xhamster.py] detail")
-
-    title = urllib.unquote_plus( params.get("title") )
-    thumbnail = urllib.unquote_plus( params.get("thumbnail") )
-    plot = urllib.unquote_plus( params.get("plot") )
+def findvideos(item):
+    logger.info("[xhamster.py] findvideos")
 
     # Descarga la página
-    data = scrapertools.cachePage(url)
+    data = scrapertools.cachePage(item.url)
 
     #    'file': '1293721834:6LvlmejCLBs/518596_koy_kizlari_2.flv',
     patron = "'file': '([^']+)'"
     matches = re.compile(patron,re.DOTALL).findall(data)
+    itemlist = []
     if len(matches)>0:
         url = "http://88.208.24.132/flv2/" + matches[0]
         server="Directo"
-        xbmctools.playvideo4(CHANNELNAME,server,url,category,title,thumbnail,plot)
-    # ----------------------------
+
+        itemlist.append( Item(channel=CHANNELNAME, action="play" , title=item.title , url=url, thumbnail=item.thumbnail, plot=item.plot, server=server, folder=False))
+
+    return itemlist
