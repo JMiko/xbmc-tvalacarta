30cf3ec95207b58a835d82f6c44cd53c55f27b6b
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-04-10 21:29:51 +0000

    Arreglado hogarutil y pasado a generico

--- trunk/tvalacarta/tvalacarta/channels/hogarutil.py
+++ trunk/tvalacarta/tvalacarta/channels/hogarutil.py
@@ -21,7 +21,6 @@ logger.info("[hogarutil.py] init")
 
 DEBUG = True
 CHANNELNAME = "hogarutil"
-CHANNELCODE = "hogarutil"
 
 def isGeneric():
     return True
@@ -31,7 +30,7 @@ def mainlist(item):
 
     itemlist = []
     itemlist.append( Item(channel=CHANNELNAME, title="Cocina"     , action="videococinalist"    , url="http://www.hogarutil.com/Cocina/Recetas+en+v%C3%ADdeo", folder=True) )
-    itemlist.append( Item(channel=CHANNELNAME, title="Bricolaje"  , action="videobricolajelist" , url="http://www.hogarutil.com/Bricomania/Trabajos+en+v%C3%ADdeo", folder=True) )
+    itemlist.append( Item(channel=CHANNELNAME, title="Bricolaje"  , action="videobricolajelist" , url="http://www.hogarutil.com/Bricomania/Tareas+en+v%C3%ADdeo", folder=True) )
     itemlist.append( Item(channel=CHANNELNAME, title="Decoración" , action="videobricolajelist" , url="http://www.hogarutil.com/Decogarden/Trabajos+en+v%C3%ADdeo", folder=True) )
     itemlist.append( Item(channel=CHANNELNAME, title="Jardineria" , action="videobricolajelist" , url="http://www.hogarutil.com/Jardineria/Trabajos+en+v%C3%ADdeo", folder=True) )
     itemlist.append( Item(channel=CHANNELNAME, title="Buscar"     , action="search"             , folder=True) )
@@ -89,41 +88,16 @@ def videococinalist(item):
     
     itemlist = []
 
-    if (params.has_key("baseurl")):
-        baseurl = urllib.unquote_plus( params.get("baseurl") )
+    if not item.extra == "":
+        baseurl = item.extra
     else:
-        baseurl = url
+        baseurl = item.url
     logger.info("[hogarutil.py] baseurl="+baseurl)
 
     # Descarga la página
-    data = scrapertools.cachePage(url)
-    #logger.info(data)
-
-    # Extrae el enlace a la página siguiente
-    #<span style="color:white" class="pagActual">2</span><a onmouseout="javascript:setDefaultColor(this);" onmouseover="javascript:setWhiteColor(this);" style="color:#5C758C; font-weight:normal;" href="?vgnextoid=550c8fc16b4bd110VgnVCM100000720d1ec2RCRD&vgnextrefresh=1&nextItem=40" class="pagRef">3</a>
-    #patronvideos  = '<a.*?href="([^"]+)"[^>]+> siguiente'
-    patronvideos = '<span.*?class="pagActual">[^<]+</span><a.*?href="([^"]+)".*?>([^<]+)</a>'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
-
-    for match in matches:
-        scrapedtitle = "Ir a página "+match[1]
-        scrapedurl = baseurl+match[0]
-        scrapedthumbnail = ""
-
-        # Depuracion
-        if (DEBUG):
-            logger.info("scrapedtitle="+scrapedtitle)
-            logger.info("scrapedurl="+scrapedurl)
-            logger.info("scrapedthumbnail="+scrapedthumbnail)
-
-        # Añade al listado de XBMC
-        addpagina( scrapedtitle , scrapedurl , baseurl , "videococinalist" )
+    data = scrapertools.cachePage(item.url)
 
     # Extrae las entradas (carpetas)
-    #patronvideos  = '<li(?: class="par")?><a href="(/[^/]+/Trabajos\+en\+v[^"]+)">([^<]+)<'
-    #patronvideos = '<li><span style="text-align:left; width:165px; margin-right:1px;" class="floatright"><img style="float:right;" src="/staticFiles/logo-hogarutil-video.gif"/>Karlos Arguiñano</span><a title="Bacalao fresco con puerros y vinagreta de sepia" href="/Cocina/Recetas+en+v%C3%ADdeo/Bacalao+fresco+con+puerros+y+vinagreta+de+sepia">Bacalao fresco con puerros y vinagreta de...</a></li>'
-    #<li class="par"><span style="text-align:left; width:165px; margin-right:1px;" class="floatright"><img style="float:right;" src="/staticFiles/logo-hogarutil-video.gif"/>Karlos Arguiñano</span><a title="Acelgas guisadas en hojaldre" href="/Cocina/Recetas+en+v%C3%ADdeo/Acelgas+guisadas+en+hojaldre">Acelgas guisadas en hojaldre</a></li><li><span style="text-align:left; width:165px; margin-right:1px;" class="floatright"><img style="float:right;" src="/staticFiles/logo-hogarutil-video.gif"/>Karlos Arguiñano</span><a title="Albóndigas a la jardinera" href="/Cocina/Recetas+en+v%C3%ADdeo/Alb%C3%B3ndigas+a+la+jardinera">Albóndigas a la jardinera</a></li>
     patronvideos = '<li.*?><span[^>]+><img.*?src="/staticFiles/logo-hogarutil-video.gif"[^>]+>([^<]+)</span><a title="([^"]+)" href="([^"]+)">[^<]+</a></li>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
@@ -136,49 +110,12 @@ def videococinalist(item):
         except:
             pass
 
-        # URL
         scrapedurl = urlparse.urljoin("http://www.hogarutil.com",urllib.quote(match[2]))
-        
-        # Thumbnail
         scrapedthumbnail = ""
-        
-        # procesa el resto
-        scrapeddescription = ""
+        scrapedplot = ""
+        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="play" , url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot , server="directo" , folder=False) )
 
-        # Depuracion
-        if (DEBUG):
-            logger.info("scrapedtitle="+scrapedtitle)
-            logger.info("scrapedurl="+scrapedurl)
-            logger.info("scrapedthumbnail="+scrapedthumbnail)
-
-        # Añade al listado de XBMC
-        addvideo( scrapedtitle , scrapedurl , category )
-
-    # Label (top-right)...
-    xbmcplugin.setPluginCategory( handle=pluginhandle, category=category )
-
-    # Disable sorting...
-    xbmcplugin.addSortMethod( handle=pluginhandle, sortMethod=xbmcplugin.SORT_METHOD_NONE )
-
-    # End of directory...
-    xbmcplugin.endOfDirectory( handle=pluginhandle, succeeded=True )
-
-def videobricolajelist(item):
-    logger.info("[hogarutil.py] videolist")
-
-    if (params.has_key("baseurl")):
-        baseurl = urllib.unquote_plus( params.get("baseurl") )
-    else:
-        baseurl = url
-
-    # Descarga la página
-    data = scrapertools.cachePage(url)
-    #logger.info(data)
-    
     # Extrae el enlace a la página siguiente
-    #<span style="color:white" class="pagActual">2</span><a onmouseout="javascript:setDefaultColor(this);" onmouseover="javascript:setWhiteColor(this);" style="color:#5C758C; font-weight:normal;" href="?vgnextoid=550c8fc16b4bd110VgnVCM100000720d1ec2RCRD&vgnextrefresh=1&nextItem=40" class="pagRef">3</a>
-    #patronvideos  = '<a.*?href="([^"]+)"[^>]+> siguiente'
-    #patronvideos = '<span.*?class="pagActual">[^<]+</span><a.*?href="([^"]+)".*?>([^<]+)</a>'
     patronvideos = '<span.*?class="pagActual">[^<]+</span><a.*?href="([^"]+)".*?>([^<]+)</a>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
@@ -187,71 +124,63 @@ def videobricolajelist(item):
         scrapedtitle = "Ir a página "+match[1]
         scrapedurl = baseurl+match[0]
         scrapedthumbnail = ""
+        scrapedplot = ""
+        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="videococinalist" , url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot , extra=baseurl) )
 
-        # Depuracion
-        if (DEBUG):
-            logger.info("scrapedtitle="+scrapedtitle)
-            logger.info("scrapedurl="+scrapedurl)
-            logger.info("scrapedthumbnail="+scrapedthumbnail)
+    return itemlist
 
-        # Añade al listado de XBMC
-        addpagina( scrapedtitle , scrapedurl , baseurl , "videobricolajelist" )
+def videobricolajelist(item):
+    logger.info("[hogarutil.py] videolist")
 
+    itemlist = []
+    if not item.extra == "":
+        baseurl = item.extra
+    else:
+        baseurl = item.url
+    logger.info("[hogarutil.py] baseurl="+baseurl)
+
+    # Descarga la página
+    data = scrapertools.cachePage(item.url)
+    
     # Extrae las entradas (carpetas)
-    #<li class="par"><span class="floatrightVideo"><img src="/staticFiles/logo-hogarutil-video.gif"/></span><a href="/Bricomania/Trabajos+en+v%C3%ADdeo/Actualizar+mueble+cl%C3%A1sico">Actualizar mueble clásico</a></li><li><span class="floatrightVideo"><img src="/staticFiles/logo-hogarutil-video.gif"/></span><a href="/Bricomania/Trabajos+en+v%C3%ADdeo/Ahorro+consumo+de+ducha">Ahorro consumo de ducha</a></li><li class="par"><span class="floatrightVideo"><img src="/staticFiles/logo-hogarutil-video.gif"/></span><a href="/Bricomania/Trabajos+en+v%C3%ADdeo/Ahorro+energ%C3%A9tico">Ahorro energético</a></li>
-    #<li class="par"><span class="floatrightVideo"><img src="/staticFiles/logo-hogarutil-video.gif"/></span><a href="/Bricomania/Trabajos+en+v%C3%ADdeo/Asiento+para+ba%C3%B1era">Asiento para bañera</a></li><li><span class="floatrightVideo"><img src="/staticFiles/logo-hogarutil-video.gif"/></span><a href="/Bricomania/Trabajos+en+v%C3%ADdeo/Balda+decorativa+infantil">Balda decorativa infantil</a></li><li class="par"><span class="floatrightVideo"><img src="/staticFiles/logo-hogarutil-video.gif"/></span><
     patronvideos = '<li.*?><span.*?><img src="/staticFiles/logo-hogarutil-video.gif"/></span><a href="([^"]+)">([^<]+)</a></li>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
     for match in matches:
-        # Titulo
         scrapedtitle = match[1]
         try:
             scrapedtitle = unicode( scrapedtitle, "utf-8" ).encode("iso-8859-1")
         except:
             pass
-
-        # URL
         scrapedurl = urlparse.urljoin("http://www.hogarutil.com",urllib.quote(match[0]))
-        
-        # Thumbnail
         scrapedthumbnail = ""
-        
-        # procesa el resto
-        scrapeddescription = ""
+        scrapedplot = ""
+        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="play" , url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot , server="directo" , folder=False) )
 
-        # Depuracion
-        if (DEBUG):
-            logger.info("scrapedtitle="+scrapedtitle)
-            logger.info("scrapedurl="+scrapedurl)
-            logger.info("scrapedthumbnail="+scrapedthumbnail)
+    # Extrae el enlace a la página siguiente
+    patronvideos = '<span.*?class="pagActual">[^<]+</span><a.*?href="([^"]+)".*?>([^<]+)</a>'
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    scrapertools.printMatches(matches)
 
-        # Añade al listado de XBMC
-        addvideo( scrapedtitle , scrapedurl , category )
+    for match in matches:
+        scrapedtitle = "Ir a página "+match[1]
+        scrapedurl = baseurl+match[0]
+        scrapedthumbnail = ""
+        scrapedplot = ""
+        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="videobricolajelist" , url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot , extra=baseurl) )
 
-    # Label (top-right)...
-    xbmcplugin.setPluginCategory( handle=pluginhandle, category=category )
-
-    # Disable sorting...
-    xbmcplugin.addSortMethod( handle=pluginhandle, sortMethod=xbmcplugin.SORT_METHOD_NONE )
-
-    # End of directory...
-    xbmcplugin.endOfDirectory( handle=pluginhandle, succeeded=True )
+    return itemlist
 
 def play(item):
     logger.info("[hogarutil.py] play")
 
-    logger.info("url="+url)
-    infotitle = unicode( xbmc.getInfoLabel( "ListItem.Title" ), "utf-8" )
-    #logger.info("[hogarutil.py] infotitle="+infotitle)
-    
-    # Abre dialogo
-    dialogWait = xbmcgui.DialogProgress()
-    dialogWait.create( 'Descargando datos del vídeo...', infotitle )
+    itemlist = []
+
+    logger.info("url="+item.url)
 
     # Averigua la descripcion (plot)
-    data = scrapertools.cachePage(url)
+    data = scrapertools.cachePage(item.url)
     patronvideos = '<meta name="BNT_RECETA_DESC" content="([^"]+)" />'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
@@ -281,24 +210,13 @@ def play(item):
         mediaurl = ""
     logger.info("[hogarutil.py] mediaurl="+mediaurl)
 
-    # Playlist vacia
-    playlist = xbmc.PlayList( xbmc.PLAYLIST_VIDEO )
-    playlist.clear()
-
-    # Crea la entrada y la añade al playlist
-    listitem = xbmcgui.ListItem( infotitle, iconImage="DefaultVideo.png", thumbnailImage=infothumbnail )
+    '''
     listitem.setProperty("SWFPlayer", "http://www.hogarutil.com/staticFiles/static/player/BigBainetPlayer.swf")
     listitem.setInfo( "video", { "Title": infotitle, "Plot" : infoplot , "Studio" : CHANNELNAME , "Genre" : category } )
     playlist.add( mediaurl, listitem )
-    #xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play("rtmp://aialanetfs.fplive.net/aialanet?slist=Jardineria/palmera-roebelen.flv", nuevoitem)
-
-    # Cierra dialogo
-    dialogWait.close()
-    del dialogWait
-
-    # Reproduce
-    xbmcPlayer = xbmc.Player( xbmc.PLAYER_CORE_AUTO )
-    xbmcPlayer.play(playlist)   
+    '''
+    itemlist.append( Item(channel=CHANNELNAME, title=item.title , action="play" , url=mediaurl, thumbnail=infothumbnail , plot=infoplot , server = "directo" , folder=False) )
+    return itemlist
 
 def directplay(item):
 
