d6d3c64629840a0e5645505051873bf1b95740e0
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-06-27 10:45:01 +0000

    Actualización canal animeid - Añadida búsqueda por género y alfabética, arreglados algunos problemas de encoding

--- pelisalacarta/pelisalacarta/channels/animeid.py
+++ pelisalacarta/pelisalacarta/channels/animeid.py
@@ -30,8 +30,87 @@ def mainlist(item):
     itemlist.append( Item(channel=CHANNELNAME, action="animelist" , title="Últimos animes agregados" , url="http://animeid.com/" ))
     itemlist.append( Item(channel=CHANNELNAME, action="airlist"   , title="Series en emisión"        , url="http://animeid.com/" ))
     itemlist.append( Item(channel=CHANNELNAME, action="fulllist"  , title="Lista completa de animes" , url="http://animeid.com/" ))
-    #itemlist.append( Item(channel=CHANNELNAME, action="genrelist" , title="Listado por genero"       , url="http://animeid.com/" ))
-    #itemlist.append( Item(channel=CHANNELNAME, action="alphalist" , title="Listado alfabetico"       , url="http://animeid.com/" ))
+    itemlist.append( Item(channel=CHANNELNAME, action="genrelist" , title="Listado por genero"       , url="http://animeid.com/" ))
+    itemlist.append( Item(channel=CHANNELNAME, action="alphalist" , title="Listado alfabetico"       , url="http://animeid.com/" ))
+
+    return itemlist
+
+def alphalist(item):
+    logger.info("[animeid.py] alphalist")
+    itemlist = []
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="0-9", url="http://animeid.com/letras/0-9.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="A"  , url="http://animeid.com/letras/a.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="B"  , url="http://animeid.com/letras/b.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="C"  , url="http://animeid.com/letras/c.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="D"  , url="http://animeid.com/letras/d.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="E"  , url="http://animeid.com/letras/e.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="F"  , url="http://animeid.com/letras/f.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="G"  , url="http://animeid.com/letras/g.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="H"  , url="http://animeid.com/letras/h.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="I"  , url="http://animeid.com/letras/i.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="J"  , url="http://animeid.com/letras/j.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="K"  , url="http://animeid.com/letras/k.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="L"  , url="http://animeid.com/letras/l.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="M"  , url="http://animeid.com/letras/m.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="N"  , url="http://animeid.com/letras/n.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="O"  , url="http://animeid.com/letras/o.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="P"  , url="http://animeid.com/letras/p.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="Q"  , url="http://animeid.com/letras/q.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="R"  , url="http://animeid.com/letras/r.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="S"  , url="http://animeid.com/letras/s.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="T"  , url="http://animeid.com/letras/t.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="U"  , url="http://animeid.com/letras/u.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="V"  , url="http://animeid.com/letras/v.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="W"  , url="http://animeid.com/letras/w.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="X"  , url="http://animeid.com/letras/x.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="Y"  , url="http://animeid.com/letras/y.html"))
+    itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title="Z"  , url="http://animeid.com/letras/z.html"))
+
+    return itemlist
+
+def genrelist(item):
+    logger.info("[animeid.py] genrelist")
+    '''
+    <div class="ctit">Animes por g&eacute;nero:</div>
+    <div class="ccon">
+    <div id="genuno"> <ul class="alfa" style="margin-top:6px; margin-left: 31px;">
+      <li><a href="/genero/accion.html" class="let">Acci&oacute;n</a></li>
+      <li><a href="/genero/aventura.html" class="let">Aventura</a></li>
+      <li><a href="/genero/carreras.html" class="let">Carreras</a></li>
+      <li><a href="/genero/comedia.html" class="let">Comedia</a></li>
+      <li><a href="/genero/cyberpunk.html" class="let">Cyberpunk</a></li> </ul> <ul class="alfa" style="margin-left: 30px;">
+      <li><a href="/genero/deportes.html" class="let">Deportes</a></li>
+      <li><a href="/genero/drama.html" class="let">Drama</a></li>
+      <li><a href="/genero/ecchi.html" class="let">Ecchi</a></li>
+      <li><a href="/genero/escolares.html" class="let">Escolares</a></li>
+      <li><a href="/genero/fantasia.html" class="let">Fantas&iacute;a</a></li>
+      <li><a href="javascript:void(0);" class="let" id="unodos"><img src="/img/ncat.png" alt="Siguiente" border="0" /></a></li> </ul> </div>
+    <div id="gendos" style="display: none; margin-top:6px;"> <ul class="alfa" style="margin-left:29px;"> <li><a href="javascript:void(0);" class="let" id="dosuno"><img src="/img/pcat.png" alt="Anterior" border="0" /></a></li> <li><a href="/genero/ficcion.html" class="let">Ficci&oacute;n</a></li> <li><a href="/genero/gore.html" class="let">Gore</a></li> <li><a href="/genero/harem.html" class="let">Harem</a></li> <li><a href="/genero/horror.html" class="let">Horror</a></li> <li><a href="/genero/josei.html" class="let">Josei</a></li> <li><a href="/genero/lucha.html" class="let">Lucha</a></li> </ul> <ul class="alfa" style="margin-left: 50px;"> <li><a href="/genero/magia.html" class="let">Magia</a></li> <li><a href="/genero/mecha.html" class="let">Mecha</a></li> <li><a href="/genero/militar.html" class="let">Militar</a></li> <li><a href="/genero/misterio.html" class="let">Misterio</a></li> <li><a href="/genero/musica.html" class="let">M&uacute;sica</a></li> <li><a href="javascript:void(0);" class="let" id="dostres"><img src="/img/ncat.png" alt="Siguiente" border="0" /></a></li> </ul> </div>
+    <div id="gentres" style="display: none; margin-top:6px;"> <ul class="alfa" style="margin-left: 5px;"> <li><a href="javascript:void(0);" class="let" id="tresdos"><img src="/img/pcat.png" alt="Anterior" border="0" /></a></li> <li><a href="/genero/parodias.html" class="let">Parodias</a></li> <li><a href="/genero/psicologico.html" class="let">Psicol&oacute;gico</a></li> <li><a href="/genero/recuentos-de-la-vida.html" class="let">Recuentos de la Vida</a></li> <li><a href="/genero/romance.html" class="let">Romance</a></li> </ul> <ul class="alfa" style="margin-left: 40px;"> <li><a href="/genero/seinen.html" class="let">Seinen</a></li> <li><a href="/genero/shojo.html" class="let">Shojo</a></li> <li><a href="/genero/shonen.html" class="let">Shonen</a></li> <li><a href="/genero/vampiros.html" class="let">Vampiros</a></li> <li><a href="/genero/yaoi.html" class="let">Yaoi</a></li> <li><a href="/genero/yuri.html" class="let">Yuri</a></li> </ul> </div>
+    </div>
+    </div>
+    '''
+    itemlist = []
+
+    # Descarga la pagina
+    data = scrapertools.cache_page(item.url)
+
+    # Extrae las entradas (carpetas)  <a class="accion linkFader" href="../accion-1.html"></a>
+    patron  = '<div id="gen[^"]+"(.*?)</div>'
+    matches2 = re.compile(patron,re.DOTALL).findall(data)
+    
+    for match2 in matches2:
+        patron = '<li><a href="([^"]+)" class="let">([^<]+)<'
+        matches = re.compile(patron,re.DOTALL).findall(match2)
+        
+        for match in matches:
+            scrapedtitle = scrapertools.unescape(match[1])
+            scrapedurl = urlparse.urljoin(item.url,match[0])
+            scrapedthumbnail = ""
+            scrapedplot = ""
+            if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+    
+            itemlist.append( Item(channel=CHANNELNAME, action="newlist" , title=scrapedtitle , url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot))
 
     return itemlist
 
