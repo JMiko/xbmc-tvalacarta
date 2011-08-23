d84822afbb120467953c6a25e2922bdc704576ca
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-08-23 01:39:27 +0000

    buenaisla - Actualizado por cambios en la web

--- pelisalacarta/pelisalacarta/channels/buenaisla.py
+++ pelisalacarta/pelisalacarta/channels/buenaisla.py
@@ -27,7 +27,7 @@ def mainlist(item):
     itemlist.append( Item(channel=CHANNELNAME, title="Novedades", action="novedades", url="http://www.buenaisla.com/modules.php?name=Anime-Online"))
     itemlist.append( Item(channel=CHANNELNAME, title="Últimas Series Agregadas" , action="ultimas", url="http://www.buenaisla.com/modules.php?name=Anime-Online"))
     itemlist.append( Item(channel=CHANNELNAME, title="Listado por Géneros", action="cat", url="http://www.buenaisla.com/modules.php?name=Anime-Online"))
-    itemlist.append( Item(channel=CHANNELNAME, title="Lista Completa" , action="listacompleta", url="http://www.buenaisla.com/modules.php?name=Anime-Online"))
+    itemlist.append( Item(channel=CHANNELNAME, title="En emisión" , action="listacompleta", url="http://www.buenaisla.com/modules.php?name=Anime-Online"))
     itemlist.append( Item(channel=CHANNELNAME, title="Buscar" , action="busqueda") )
 
     return itemlist
@@ -39,19 +39,18 @@ def novedades(item):
     data = scrapertools.cachePage(item.url)
 
     # Extrae las entradas
-    patronvideos  = '<td width="33%">(.*?)</td>'
+    patronvideos  = '<td width="50%"(.*?)</td>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     if DEBUG: scrapertools.printMatches(matches)
-
     itemlist = []
     for elemento in matches:
-        patronvideos = '<a href="(.*?)">.*?<img alt="(.*?)" src="(.*?)".*?></a>'
+        patronvideos = '<img src="([^"]+)".*?<a.*?href="(.*?)">(.*?)</a>'
         matches2 = re.compile(patronvideos,re.DOTALL).findall(elemento)
 
         for match in matches2:
-            scrapedurl = urlparse.urljoin("http://www.buenaisla.com/",match[0])
-            scrapedtitle = match[1]
-            scrapedthumbnail = urlparse.urljoin("http://www.buenaisla.com/",match[2])
+            scrapedurl = urlparse.urljoin("http://www.buenaisla.com/",match[1])
+            scrapedtitle = match[2]
+            scrapedthumbnail = urlparse.urljoin("http://www.buenaisla.com/",match[0])
             scrapedplot = ""
             logger.info(scrapedtitle)
 
@@ -83,7 +82,7 @@ def videos(item):
         invalid = video[1]
         invalid = invalid[0:8]
         if invalid!= "FN3WE43K" and invalid!="9CC3F8&e":
-            scrapedtitle = title.strip() + " - " + video[0]
+            scrapedtitle = video[0]
             videourl = video[1]
             server = video[2]
             if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+videourl+"], thumbnail=["+scrapedthumbnail+"]")
@@ -94,18 +93,51 @@ def videos(item):
     return itemlist
 
 def cat(item):
-
     logger.info("[islapeliculas.py] categorias")
+    itemlist = []
 
     # Descarga la página
     data = scrapertools.cachePage(item.url)
 
     # Extrae las entradas
-    itemlist = []
-    matches=["Accion","Artes-Marciales","Aventura","Ciencia-Ficcion","Comedia","Ecchi","Escolares","Deportes","Drama","Fantasia","Harem","Horror","Magicos","Misterio","Romance","Seinen","Shojo","Shonen","Vampiros"]
+    '''
+    <td class="bordesTt">G&eacute;neros</td>    
+    </tr>
+    <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="5">
+    <tr>
+    <td width="20%"><a class="h2_2" href="genero-accion">Accion</a></td>
+    <td width="20%"><a class="h2_2" href="genero-aventura">Aventura</a></td>
+    <td width="20%"><a class="h2_2" href="genero-ciencia-ficcion">Ciencia Ficcion </a></td>
+    <td width="20%"><a class="h2_2" href="genero-comedia">Comedia</a></td>
+    <td width="20%"><a class="h2_2" href="genero-drama">Drama</a></td>
+    </tr>
+    <tr>
+    <td><a class="h2_2" href="genero-ecchi">Ecchi</a></td>
+    <td><a class="h2_2" href="genero-escolares">Escolares</a></td>
+    <td><a class="h2_2" href="genero-horror">Horror</a></td>    
+    <td><a class="h2_2" href="genero-misterio">Misterio</a></td>
+    <td><a class="h2_2" href="genero-harem">Harem</a></td>
+    </tr>
+    <tr>
+    <td><a class="h2_2" href="genero-romance">Romance</a></td>
+    <td><a class="h2_2" href="genero-seinen">Seinen</a></td>
+    <td><a class="h2_2" href="genero-shojo">Shojo</a></td>
+    
+    <td><a class="h2_2" href="genero-shonen">Shonen</a></td>
+    <td><a class="h2_2" href="genero-yuri">Yuri</a></td>
+    </tr>
+    </table>
+    '''
+    patron = '<td class="bordesTt">G&eacute;neros(.*?)</table>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    data = matches[0]
+    patron = '<a class="h2_2" href="([^"]+)">([^<]+)</a>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    
     for match in matches:
-        scrapedurl = "http://www.buenaisla.com/modules.php?name=Anime-Online&func=Generos&Genero=" + match
-        scrapedtitle = match
+        scrapedurl = urlparse.urljoin("http://www.buenaisla.com",match[0])
+        scrapedtitle = match[1]
         scrapedtitle = scrapedtitle.replace("-"," ")
         scrapedthumbnail = ""
         scrapedplot = ""
@@ -123,19 +155,19 @@ def listaseries(item):
     data = scrapertools.cachePage(item.url)
 
     # Extrae las entradas
-    patronvideos  = '<td  align="center" width=.*?33%.*?>(.*?)</td>'
+    patronvideos  = '<td width="33%"(.*?)</h2>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     if DEBUG: scrapertools.printMatches(matches)
 
     itemlist = []
     for elemento in matches:
-        patronvideos = '<a href="([^"]+)".*?><img alt="Capitulos de ([^"]+)".*?src=\'(.*?)\'.*?</a>'
+        patronvideos = '<a.*?href="([^"]+)">.*?<img src="([^<]+)" alt="([^"]+)"'
         matches2 = re.compile(patronvideos,re.DOTALL).findall(elemento)
 
         for match in matches2:
             scrapedurl = urlparse.urljoin("http://www.buenaisla.com/",match[0])
-            scrapedtitle = match[1]
-            scrapedthumbnail = urlparse.urljoin("http://www.buenaisla.com/",match[2])
+            scrapedtitle = match[2]
+            scrapedthumbnail = urlparse.urljoin("http://www.buenaisla.com/",match[1])
             scrapedplot = ""
             logger.info(scrapedtitle)
 
@@ -151,19 +183,27 @@ def ultimas(item):
     data = scrapertools.cachePage(item.url)
 
     # Extrae las entradas
-    patronvideos  = '<h2>Ultimas series Agregadas(.*?)<td class="tbll">'
+    '''
+    <td width="33%"  align="center" valign="top">
+    <a class="h2_5" href="ver-anime-kamisama-dolls-castellano-784">
+    <div>
+    <img src="images/series/784.png" alt="Kamisama Dolls" class="img_capitulo" />    
+    </div></a>
+    <br /> <h2><a class="h2_5" href="ver-anime-kamisama-dolls-castellano-784">Kamisama Dolls</a></h2>
+    '''
+    patronvideos  = '<td width="33%"(.*?)</h2>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     if DEBUG: scrapertools.printMatches(matches)
 
     itemlist = []
     for elemento in matches:
-        patronvideos = '<a href="([^"]+)".*?><img alt="Capitulos de ([^"]+)".*?src=\'(.*?)\'.*?</a><br><a.*?>.*?</a>.*?<td.*?>'
+        patronvideos = '<a.*?href="([^"]+)">.*?<img src="([^<]+)" alt="([^"]+)"'
         matches2 = re.compile(patronvideos,re.DOTALL).findall(elemento)
 
         for match in matches2:
             scrapedurl = urlparse.urljoin("http://www.buenaisla.com/",match[0])
-            scrapedtitle = match[1]
-            scrapedthumbnail = urlparse.urljoin("http://www.buenaisla.com/",match[2])
+            scrapedtitle = match[2]
+            scrapedthumbnail = urlparse.urljoin("http://www.buenaisla.com/",match[1])
             scrapedplot = ""
             logger.info(scrapedtitle)
 
@@ -177,20 +217,39 @@ def listacapitulos(item):
 
     # Descarga la página
     data = scrapertools.cachePage(item.url)
-    # Extrae las entradas
-    patronvideos  = '<a.*?alt="Capitulos de.*?".*?>.*?<img.*?src="(.*?)".*?SINOPSIS: </strong>(.*?)<b>'
+    
+    # Extrae el thumbnail y el argumento
+    patronvideos  = '<img class="img_capitulo" alt="[^"]*" src="([^"]+)" />'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    if DEBUG: scrapertools.printMatches(matches)
-    if len (matches)>0:
-            imagen = "http://www.buenaisla.com/"+matches[0][0]
-            sinopsis = matches[0][1]
+    if len(matches)>0:
+        imagen = "http://www.buenaisla.com/"+matches[0]
+    
+    patronvideos  = '(<div id="comentariocompleto" align="justify" style="display:none">.*?)</div>'
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    if len(matches)>0:
+        sinopsis = scrapertools.htmlclean(matches[0])
 
     # Extrae las entradas
-    patronvideos  = '<td height="25">(.*?)</td>'
+    '''
+    <p class="accContent" >
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-1-21821">Getsumen To Heiki Mina capitulo 1 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-2-21822">Getsumen To Heiki Mina capitulo 2 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-3-21824">Getsumen To Heiki Mina capitulo 3 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-4-21825">Getsumen To Heiki Mina capitulo 4 </a>    
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-5-21826">Getsumen To Heiki Mina capitulo 5 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-6-21827">Getsumen To Heiki Mina capitulo 6 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-7-21828">Getsumen To Heiki Mina capitulo 7 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-8-21829">Getsumen To Heiki Mina capitulo 8 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-9-21830">Getsumen To Heiki Mina capitulo 9 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-10-21831">Getsumen To Heiki Mina capitulo 10 </a>
+    <a class="h2_2" href="online-capitulo-anime-espanol-getsumen-to-heiki-mina-11-21832">Getsumen To Heiki Mina capitulo 11 </a>
+    </p>
+    '''
+    patronvideos  = '<p class="accContent"(.*?)</p>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     itemlist = []
     for elemento in matches:
-        patronvideos = '.*?<a href="(.*?)".*?>(.*?)</a>'
+        patronvideos = '<a.*?href="([^"]+)">([^<]+)</a>'
         matches2 = re.compile(patronvideos,re.DOTALL).findall(elemento)
 
         for match in matches2:
@@ -227,24 +286,22 @@ def listacompleta(item):
     data = scrapertools.cachePage(item.url)
 
     # Extrae las entradas
-    patronvideos  = '<td bgcolor="#DEEBFE">(.*?)</td>'
+    patronvideos  = '<li>[^<]+'
+    patronvideos += '<a href="([^"]+)">[^<]+'
+    patronvideos += '<img src="([^"]+)"[^>]+>([^<]+)</a>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
     if DEBUG: scrapertools.printMatches(matches)
 
     itemlist = []
-    for elemento in matches:
-        patronvideos = '<a.*?href="(.*?)".*?>.*?>(.*?)</div></a>'
-        matches2 = re.compile(patronvideos,re.DOTALL).findall(elemento)
+    for match in matches:
+        scrapedurl = urlparse.urljoin(item.url,match[0])
+        scrapedtitle = match[2].strip()
+        scrapedthumbnail = urlparse.urljoin(item.url,match[1])
+        scrapedplot = ""
+        logger.info(scrapedtitle)
 
-        for match in matches2:
-            scrapedurl = urlparse.urljoin("http://www.buenaisla.com/",match[0])
-            scrapedtitle = match[1]
-            scrapedthumbnail = ""
-            scrapedplot = ""
-            logger.info(scrapedtitle)
-
-            # Añade al listado
-            itemlist.append( Item(channel=CHANNELNAME, action="listacapitulos", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+        # Añade al listado
+        itemlist.append( Item(channel=CHANNELNAME, action="listacapitulos", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
 
     return itemlist
 
