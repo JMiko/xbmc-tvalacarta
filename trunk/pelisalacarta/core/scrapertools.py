397c4d92546886ee49a79aad67b076adc9a9460c
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-10-13 17:47:56 +0000

    Ahora la cabecera "Location" se obtiene también con cookies

--- trunk/pelisalacarta/core/scrapertools.py
+++ trunk/pelisalacarta/core/scrapertools.py
@@ -22,7 +22,7 @@ import md5
 import os
 import config
 import logger
-import re, htmlentitydefs
+import re
 import downloadtools
 
 logger.info("[scrapertools.py] init")
@@ -289,7 +289,18 @@ def cachePagePost(url,post):
     '''
     return data
 
-def downloadpage(url,post=None,headers=[['User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12']]):
+class NoRedirectHandler(urllib2.HTTPRedirectHandler):
+    def http_error_302(self, req, fp, code, msg, headers):
+        infourl = urllib.addinfourl(fp, headers, req.get_full_url())
+        infourl.status = code
+        infourl.code = code
+        return infourl
+    http_error_300 = http_error_302
+    http_error_301 = http_error_302
+    http_error_303 = http_error_302
+    http_error_307 = http_error_302
+
+def downloadpage(url,post=None,headers=[['User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12']],follow_redirects=True):
     logger.info("[scrapertools.py] downloadpage")
     logger.info("[scrapertools.py] url="+url)
     
@@ -361,7 +372,10 @@ def downloadpage(url,post=None,headers=[['User-Agent', 'Mozilla/5.0 (Macintosh; 
             # if we use cookielib
             # then we get the HTTPCookieProcessor
             # and install the opener in urllib2
-            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+            if not follow_redirects:
+                opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj),NoRedirectHandler())
+            else:
+                opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
             urllib2.install_opener(opener)
 
         else:
@@ -704,9 +718,12 @@ def unescape(text):
                     print text[1:-1]
                     text = unichr(htmlentitydefs.name2codepoint[text[1:-1]]).encode("utf-8")
                 '''
+                import htmlentitydefs
                 text = unichr(htmlentitydefs.name2codepoint[text[1:-1]]).encode("utf-8")
             except KeyError:
                 print "keyerror"
+                pass
+            except:
                 pass
         return text # leave as is
     return re.sub("&#?\w+;", fixup, text)
@@ -863,45 +880,82 @@ def remove_show_from_title(title,show):
 def getRandom(str):
     return binascii.hexlify(md5.new(str).digest())
 
-def getLocationHeaderFromResponse(url):
-    logger.info("[scrapertools.py] getLocationHeaderFromResponse")
+def getLocationHeaderFromResponse(url,post=None,headers=[['User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12']]):
+    logger.info("[scrapertools.py] getLocationHeaderFromResponse url="+url)
 
-    if url=='':
-        return None
-
-    import httplib
-    parsedurl = urlparse.urlparse(url)
-    #print "parsedurl=",parsedurl
-
-    try:
-        host = parsedurl.netloc
-    except:
-        host = parsedurl[1]
-    #print "host=",host
-
-    try:
-        #print "1"
-        query = parsedurl.path+";"+parsedurl.query
-    except:
-        #print "2"
-        query = parsedurl[2]+";"+parsedurl[3]+"?"
-    #print "query=",query
-    query = urllib.unquote( query )
-    #print "query = " + query
-
-    import httplib
-    conn = httplib.HTTPConnection(host)
-    conn.request("GET", query)
-    response = conn.getresponse()
-    location = response.getheader("location")
-    conn.close()
+    if post is not None:
+        logger.info("[scrapertools.py] post="+post)
+    else:
+        logger.info("[scrapertools.py] post=None")
     
-    #print "location=",location
+    #  Inicializa la librería de las cookies
+    ficherocookies = os.path.join( config.get_setting("cookies.dir"), 'cookies.lwp' )
+    logger.info("[scrapertools.py] ficherocookies="+ficherocookies)
 
-    #if location!=None:
-    #    print "Encontrado header location"
+    cj = None
+    ClientCookie = None
+    cookielib = None
+
+    import cookielib
+    # importing cookielib worked
+    urlopen = urllib2.urlopen
+    Request = urllib2.Request
+    cj = cookielib.LWPCookieJar()
+    # This is a subclass of FileCookieJar
+    # that has useful load and save methods
+
+    if os.path.isfile(ficherocookies):
+        logger.info("[scrapertools.py] Leyendo fichero cookies")
+        # if we have a cookie file already saved
+        # then load the cookies into the Cookie Jar
+        cj.load(ficherocookies)
+
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj),NoRedirectHandler())
+    urllib2.install_opener(opener)
+
+    # Contador
+    inicio = time.clock()
+
+    # Diccionario para las cabeceras
+    txheaders = {}
+
+    # Construye el request
+    if post is None:
+        logger.info("[scrapertools.py] petición GET")
+    else:
+        logger.info("[scrapertools.py] petición POST")
     
-    return location
+    # Añade las cabeceras
+    logger.info("[scrapertools.py] ---------------------------")
+    for header in headers:
+        logger.info("[scrapertools.py] header=%s" % str(header[0]))
+        txheaders[header[0]]=header[1]
+    logger.info("[scrapertools.py] ---------------------------")
+
+    req = Request(url, post, txheaders)
+    handle = urlopen(req)
+    
+    # Actualiza el almacén de cookies
+    cj.save(ficherocookies)
+
+    # Lee los datos y cierra
+    #data=handle.read()
+    info = handle.info()
+    logger.info("[scrapertools.py] Respuesta")
+    logger.info("[scrapertools.py] ---------------------------")
+    location_header=""
+    for header in info:
+        logger.info("[scrapertools.py] "+header+"="+info[header])
+        if header=="location":
+            location_header=info[header]
+    handle.close()
+    logger.info("[scrapertools.py] ---------------------------")
+
+    # Tiempo transcurrido
+    fin = time.clock()
+    logger.info("[scrapertools.py] Descargado en %d segundos " % (fin-inicio+1))
+
+    return location_header
 
 def unseo(cadena):
     if cadena.upper().startswith("VER GRATIS LA PELICULA "):
