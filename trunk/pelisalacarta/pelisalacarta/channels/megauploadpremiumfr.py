201bcef1853bc8c2eabe6dc1672add9b67c4daec
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-10-14 22:27:09 +0000

    Nuevo canal megauploadpremiumfr

---
+++ trunk/pelisalacarta/pelisalacarta/channels/megauploadpremiumfr.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Canal para megauploadpremiumfr
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#------------------------------------------------------------
+import urlparse,urllib2,urllib,re
+import os, sys
+
+from core import logger
+from core import config
+from core import scrapertools
+from core.item import Item
+from servers import servertools
+
+CHANNELNAME = "megauploadpremiumfr"
+DEBUG = True
+
+def isGeneric():
+    return True
+
+def mainlist(item):
+    logger.info("[megauploadpremiumfr.py] mainlist")
+    
+    itemlist=[]
+    itemlist.append( Item(channel=CHANNELNAME, title="TV Shows - Full listing"   , action="completo" , url="http://www.megaupload-premium.com/toutes-les-series/"))
+
+    return itemlist
+
+def completo(item):
+    logger.info("[megauploadpremiumfr.py] completo")
+    itemlist = []
+
+    # Descarga la página
+    data = scrapertools.cachePage(item.url)
+    patron  = '<div class="azindex">(.*?)</div>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    
+    if len(matches)==0:
+        return itemlist
+    
+    data = matches[0]
+    patron = '<li[^>]*><a href="([^"]+)"><span class="head">([^<]+)</span></a></li>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+
+    for match in matches:
+        scrapedtitle = match[1]
+        scrapedplot = ""
+        scrapedurl = urlparse.urljoin(item.url,match[0])
+        scrapedthumbnail = ""
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+        itemlist.append( Item(channel=CHANNELNAME, action="findvideos", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+
+    return itemlist
