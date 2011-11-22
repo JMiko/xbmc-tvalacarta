c1f8f648dc046201e86dceff06f7d0319a03659b
carlescarmonacalpe@gmail.com <carlescarmonacalpe@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-11-22 19:26:03 +0000

    pelispekes.py arreglado :D

--- pelisalacarta/pelisalacarta/channels/pelispekes.py
+++ pelisalacarta/pelisalacarta/channels/pelispekes.py
@@ -1,92 +1,73 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 #------------------------------------------------------------
 # pelisalacarta - XBMC Plugin
 # Canal para pelispekes
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 #------------------------------------------------------------
 import urlparse,urllib2,urllib,re
-import os
-import sys
+import os, sys
 
-from core import scrapertools
 from core import logger
 from core import config
+from core import scrapertools
 from core.item import Item
-from platform.xbmc import xbmctools
-from pelisalacarta import buscador
-
 from servers import servertools
-from servers import vk
-
-import xbmc
-import xbmcgui
-import xbmcplugin
 
 CHANNELNAME = "pelispekes"
-
-# Esto permite su ejecución en modo emulado
-try:
-    pluginhandle = int( sys.argv[ 1 ] )
-except:
-    pluginhandle = ""
-
-# Traza el inicio del canal
-logger.info("[pelispekes.py] init")
-
 DEBUG = True
 
-def mainlist(params,url,category):
+
+
+def isGeneric():
+    return True
+
+def mainlist(item):
     logger.info("[pelispekes.py] mainlist")
+    itemlist=[]
 
-    # Añade al listado de XBMC
-    xbmctools.addnewfolder( CHANNELNAME , "novedades" , category , "Novedades" ,"http://pelispekes.com/","","")
-    xbmctools.addnewfolder( CHANNELNAME , "Listcat" , category , "Categorias" ,"http://pelispekes.com/","","")
-
-    # Cierra el directorio
-    xbmcplugin.setPluginCategory( handle=int( sys.argv[ 1 ] ), category=category )
-    xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_NONE )
-    xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True )
-
+    itemlist.append( Item(channel=CHANNELNAME , action="Generico"        , title="Novedades"                      , url="http://pelispekes.com/"))
+    itemlist.append( Item(channel=CHANNELNAME , action="Categorias"        , title="Categorias"                      , url="http://pelispekes.com/"))
+    itemlist.append( Item(channel=CHANNELNAME , action="Abecedario"        , title="Abecedario"                      , url="http://pelispekes.com/"))
     
-def novedades(params,url,category):
-    logger.info("[pelispekes.py] novedades")
 
-    # ------------------------------------------------------
-    # Descarga la página
-    # ------------------------------------------------------
+    return itemlist
+
+
+def Generico(item):
+    logger.info("[filmixt.py] Generico")
+
+    url = item.url
+                
     data = scrapertools.cachePage(url)
-    #logger.info(data)
 
-    # ------------------------------------------------------
-    # Extrae las películas
-    # ------------------------------------------------------
-    #patron  = '<div class="thumb">[^<]+<a href="([^"]+)"><img src="([^"]+)".*?alt="([^"]+)"/></a>'
-    patron  = '<div class="post-content clearfix">.+?'
-    patron += '<a href="([^"]+)">'           #Url
-    patron += '<img src="([^"]+)".+?'        #Caratula
-    patron += 'alt="([^"]+)"/></a>.+?'          #Titulo
-    patron += '<p>([^<]+)</p>'
+    # Extrae las entradas (carpetas)
+   
+    patron = 'class="filmgal">(.*?)<strong>DuraciÃ³n: </strong>'
     matches = re.compile(patron,re.DOTALL).findall(data)
-    if DEBUG: scrapertools.printMatches(matches)
+    logger.info("hay %d matches" % len(matches))
+    
 
+    itemlist = []
     for match in matches:
-        scrapedtitle = match[2]
-        scrapedurl = match[0]
-        scrapedthumbnail = match[1].replace(" ","%20")
-        scrapedplot = match[3]
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
-        try:
-            print scrapedtitle
-            scrapedtitle = scrapedtitle
-            #scrapedtitle = unicode(scrapedtitle, "utf-8" )
-            
-        except:
-            pass
-        # Añade al listado de XBMC
-        xbmctools.addnewfolder( CHANNELNAME , "listmirrors" , category , scrapedtitle , scrapedurl , scrapedthumbnail, scrapedplot )
+        patron  = '<a target="_blank" href="(.*?)">.*?'
+        patron += '<img width="145" height="199" border="0" src="(.*?)" alt="(.*?)"/>.*?'
+        patron += '<strong>Sinopsis:</strong>(.*?)</div>'
+        matches2 = re.compile(patron,re.DOTALL).findall(match)
+        logger.info("hay %d matches2" % len(matches2))
 
+        for match2 in matches2:
+        # Atributos
+            scrapedurl = match2[0]
+            scrapedtitle =match2[2].replace("Ver pelicula","").replace("&#8211;","")
+            scrapedthumbnail = match2[1]
+            scrapedplot = match2[3]
+            if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+            
+            # Aï¿½ade al listado de XBMC
+            itemlist.append( Item(channel=item.channel , action="buscavideos"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
+    
     # ------------------------------------------------------
-    # Extrae la página siguiente
+    # Extrae la p?gina siguiente
     # ------------------------------------------------------
     #patron = '<a href="([^"]+)" >\&raquo\;</a>'
     patron  = "class='current'>[^<]+</span><a href='([^']+)'"
@@ -101,210 +82,94 @@ def novedades(params,url,category):
         scrapeddescription = ""
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
 
-        # Añade al listado de XBMC
-        xbmctools.addthumbnailfolder( CHANNELNAME , scrapedtitle , scrapedurl , scrapedthumbnail, "novedades" )
+        itemlist.append( Item(channel=item.channel , action="Generico"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
+    
+    
+    return itemlist
 
-    # Label (top-right)...
-    xbmcplugin.setPluginCategory( handle=pluginhandle, category=category )
-    xbmcplugin.addSortMethod( handle=pluginhandle, sortMethod=xbmcplugin.SORT_METHOD_NONE )
-    xbmcplugin.endOfDirectory( handle=pluginhandle, succeeded=True )
+def buscavideos(item):
+    logger.info("[pelispekes.py] BuscaVideos")
 
-def Listcat(params,url,category):
-    logger.info("[pelispekes.py] Listcat")
-
-    # Descarga la página
+    url = item.url
+                
     data = scrapertools.cachePage(url)
-    #logger.info(data)
-
-    # Patron de las entradas
-    #<li  class="cat-item cat-item-15"><a href="http://pelispekes.com/categoria/accion/" title="Ver todas las entradas archivadas en Accion" class="fadeThis"><span class="entry">Accion <span class="details inline">(39)</span></span></a><a class="rss bubble" href="http://pelispekes.com/categoria/accion/feed/" title="XML"></a>
-
-    patron = '<li  class="[^"]+"><a href="([^"]+)"[^>]+>'
-    patron += '<span class="entry">([^<]+)<span class="details inline">([^<]+)</span'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
-
-    # Añade las entradas encontradas
-    for match in matches:
-        # Atributos
-        scrapedtitle = match[1]+match[2]
-        scrapedurl = match[0]
-        scrapedthumbnail = ""
-        scrapedplot = ""
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
-
-        # Añade al listado de XBMC
-        xbmctools.addnewfolder( CHANNELNAME , "novedades" , category , scrapedtitle , scrapedurl , scrapedthumbnail, scrapedplot )
-
-    # Asigna el título, desactiva la ordenación, y cierra el directorio
-    xbmcplugin.setPluginCategory( handle=pluginhandle, category=category )
-    xbmcplugin.addSortMethod( handle=pluginhandle, sortMethod=xbmcplugin.SORT_METHOD_NONE )
-    xbmcplugin.endOfDirectory( handle=pluginhandle, succeeded=True )
-        
-
-def listmirrors(params,url,category):
-    logger.info("[pelispekes.py] listmirrors")
-
-    title = unicode( xbmc.getInfoLabel( "ListItem.Title" ), "utf-8" )
     
-    thumbnail = urllib.unquote_plus( params.get("thumbnail") )
-    plot = unicode( xbmc.getInfoLabel( "ListItem.Plot" ), "utf-8" )
-    
-
-    # Descarga la página de detalle
-    # http://pelispekes.com/sorority-row/
-    data = scrapertools.cachePage(url)
-    #logger.info(data)
-    
-    # Extrae el argumento
-    patron = '<div class="sinopsis">.*?<li>(.*?)</li>'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    if len(matches)>0:
-        plot = matches[0]
-
-    
-    
-
-    patron = '<div class="page-navigation">(.*?)</table>'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
-    scrapedtitle = title
-    scrapedthumbnail = thumbnail
-    scrapedplot = plot
-    
-    if len(matches)>0:
-        data = matches[0]
-        patron  = '<a href="([^"]+)"[^>]+>([^<]+)</a>'
-        matches = re.compile(patron,re.DOTALL).findall(data)
-        scrapertools.printMatches(matches)
-        
-        for match in matches:
-            
-            
-            
-            if match[0].endswith(".html"):
-                if "/vk/" in match[0]:  #http://pelispekes.com/vk/11223192/674072850/ml3mp2pm9v00nmp2/predators-online.html
-                    patron = "http\:\/\/pelispekes.com\/vk\/([^\/]+)\/([^\/]+)\/([^\/]+)\/[^\.]+\.html"
-                    matchesvk = re.compile(patron).findall(match[0])
-                    scrapedurl = "http://pelispekes.com/modulos/embed/vkontakteX.php?oid=%s&id=%s&hash=%s" %(matchesvk[0][0],matchesvk[0][1],matchesvk[0][2])
-                    server = "Directo"
-                    xbmctools.addnewvideo( CHANNELNAME , "play" , category ,server, scrapedtitle+" - %s [VK]" %match[1] , scrapedurl , scrapedthumbnail, scrapedplot )
-                              
-            
-            
-                patron   = "http://pelispekes.com/([^/]+)/([^/]+)/[^\.]+.html"    #http://pelispekes.com/playlist/6917/el-equipo-a-online.html
-                matches2 = re.compile(patron,re.DOTALL).findall(match[0])
-                                
-                if matches2[0][0] == "playlist":
-                    xmlurl = "http://pelispekes.com/xml/%s.xml" %matches2[0][1]
-                    xmldata = scrapertools.cachePage(xmlurl)
-                    logger.info("xmldata="+xmldata)
-                    patronvideos  = '<track>[^<]+'
-                    patronvideos += '<creator>([^<]+)</creator>[^<]+'
-                    patronvideos += '<location>([^<]+)</location>.*?'
-                    patronvideos += '</track>'
-                    matchesxml = re.compile(patronvideos,re.DOTALL).findall(xmldata)
-                    scrapertools.printMatches(matchesxml)
-                    for xmlmatch in matchesxml:
-                        scrapedurl = xmlmatch[1]
-                        #xbmctools.addnewvideo( CHANNELNAME , "play" , category , server , (title.strip() + " (%d) " + videotitle) % j , url , thumbnail , plot )
-                        server = "Directo"
-                        xbmctools.addnewvideo( CHANNELNAME , "play" , category ,server, scrapedtitle+" - %s [Directo]" %xmlmatch[0] , scrapedurl , scrapedthumbnail, scrapedplot )
-                elif matches2[0][0] == "flash":
-                    scrapedurl = matches2[0][1]
-                    server = "Megavideo"
-                    scrapedtitle = scrapedtitle+" - %s" %match[1]
-                    scrapedtitle = scrapedtitle.replace("&ntilde;","ñ")
-                    xbmctools.addnewvideo( CHANNELNAME , "play" , category ,server, scrapedtitle , scrapedurl , scrapedthumbnail, scrapedplot )
-            elif "vk.php" in match[0]:
-                scrapedurl = "http://pelispekes.com/modulos/vkontakteX.php?%s" %match[0].split("?")[1]
-                server = "Directo"
-                xbmctools.addnewvideo( CHANNELNAME , "play" , category ,server, scrapedtitle+" - %s [VK]" %match[1] , scrapedurl , scrapedthumbnail, scrapedplot )
-    
-    patronvideos = '<iframe src="(http://vk[^/]+/video_ext.php[^"]+)"'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    if len(matches)>0:
-        print " encontro VKontakte.ru :%s" %matches[0]
-        scrapedurl =     vk.geturl(matches[0])    
-        server = "Directo"
-        xbmctools.addnewvideo( CHANNELNAME , "play" , category ,server, scrapedtitle+" - [VK]" , scrapedurl , scrapedthumbnail, scrapedplot )
-
-    patronvideos = '<iframe src="(http://pelispekes.com/modulos/vkontakteX.php[^"]+)"'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    if len(matches)>0:
-        print " encontro VKontakte.ru :%s" %matches[0]
-        #scrapedtitle = scrapedtitle.replace("\xf3","ñ")
-        scrapedurl =     matches[0]    
-        server = "Directo"
-        xbmctools.addnewvideo( CHANNELNAME , "play" , category ,server, scrapedtitle+" - [VK]" , scrapedurl , scrapedthumbnail, scrapedplot )    
-    # ------------------------------------------------------------------------------------
-    # Busca los enlaces a los videos en los servidores habilitados
-    # ------------------------------------------------------------------------------------
+    logger.info(data)
+    # Usa findvideos    
     listavideos = servertools.findvideos(data)
-
+    
+    itemlist = []
+    
     for video in listavideos:
-        
-        videotitle = video[0]
-        url = video[1]
         server = video[2]
-        from core import downloadtools
-        scrapedplot = downloadtools.limpia_nombre_excepto_1(scrapedplot)
-        xbmctools.addnewvideo( CHANNELNAME , "play" , category , server , scrapedtitle + " - " + videotitle , url , scrapedthumbnail , scrapedplot )
+        scrapedtitle = item.title + " [" + server + "]"
+        scrapedurl = video[1]
         
-    # Cierra el directorio
-    xbmcplugin.setPluginCategory( handle=pluginhandle, category=category )
-    xbmcplugin.addSortMethod( handle=pluginhandle, sortMethod=xbmcplugin.SORT_METHOD_NONE )
-    xbmcplugin.endOfDirectory( handle=pluginhandle, succeeded=True )
+        itemlist.append( Item(channel=CHANNELNAME, action="play" , title=scrapedtitle , url=scrapedurl, thumbnail=item.thumbnail, plot=item.plot, server=server, folder=False))
 
-def play(params,url,category):
-    logger.info("[pelispekes.py] play")
-    try:
-        title = unicode( xbmc.getInfoLabel( "ListItem.Title" ), "utf-8" )
-    except:
-        title = urllib.unquote_plus( params.get("title") )
-    thumbnail = urllib.unquote_plus( params.get("thumbnail") )
-    plot = unicode( xbmc.getInfoLabel( "ListItem.Plot" ), "utf-8" )
-    server = params.get("server")
+    return itemlist
 
-    # Abre dialogo
-    dialogWait = xbmcgui.DialogProgress()
-    dialogWait.create( 'Accediendo al video...', title , plot )
+def Categorias(item):
+    logger.info("[filmixt.py] Categorias")
 
-    # Descarga la página del reproductor
-    # http://pelispekes.com/modulos/player.php?url=dmlkZW8uYWsuZmFjZWJvb2suY29tL2Nmcy1hay1hc2gyLzMzMjM2LzY4NS8xMDU1NTcxNDYxNjI3MjNfMzQ5ODk=
-    # http://pelispekes.com/modulos/embed/playerembed.php?url=dmlkZW8uYWsuZmFjZWJvb2suY29tL2Nmcy1hay1hc2gyLzMzMjM2LzY4NS8xMDU1NTcxNDYxNjI3MjNfMzQ5ODk=
-    logger.info("[pelispekes.py] url="+url)
+    url = item.url
+                
+    data = scrapertools.cachePage(url)
+
+    # Extrae las entradas (carpetas)
+   
+    patron = '<div id="genero">(.*?)<div class="corte"></div>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    logger.info("hay %d matches" % len(matches))
     
-    ## --------------------------------------------------------------------------------------##
-    #            Busca enlaces de videos para el servidor vkontakte.ru                        #
-    ## --------------------------------------------------------------------------------------##
-    #"http://vkontakte.ru/video_ext.php?oid=89710542&id=147003951&hash=28845bd3be717e11&hd=1
-    
-    
-    if "vkontakteX.php" in url:
-        data = scrapertools.cachePage(url)
-        server = "Directo"
-        '''
-        var video_host = 'http://cs12916.vkontakte.ru/';
-        var video_uid = '87155741';
-        var video_vtag = 'fc697084d3';
-        var video_no_flv = 1;
-        var video_max_hd = '1'
-        '''
-        patronvideos = '<iframe src="(http://vk[^/]+/video_ext.php[^"]+)"'
-        matches = re.compile(patronvideos,re.DOTALL).findall(data)
-        if len(matches)>0:
-            print " encontro VKontakte.ru :%s" %matches[0]
-            url =     vk.geturl(matches[0])
+    itemlist = []
+    for match in matches:
+        patron  = '<a href="(.*?)">(.*?)</a>'
+        matches2 = re.compile(patron,re.DOTALL).findall(match)
+        logger.info("hay %d matches2" % len(matches2))
+
+        for match2 in matches2:
+        # Atributos
+            scrapedurl = "http://pelispekes.com"+match2[0]
+            scrapedtitle =match2[1]
+            scrapedthumbnail = ""
+            scrapedplot = ""
+            if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
             
-    
-    # Cierra dialogo
-    dialogWait.close()
-    del dialogWait
-
-    if len(url)>0:
+            # Aï¿½ade al listado de XBMC
+            itemlist.append( Item(channel=item.channel , action="Generico"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
         
-        logger.info("url="+url)
-        xbmctools.play_video(CHANNELNAME,server,url,category,title,thumbnail,plot)
-    else:
-        xbmctools.alertnodisponible()
+    return itemlist
+
+def Abecedario(item):
+    logger.info("[filmixt.py] Abecedario")
+
+    url = item.url
+                
+    data = scrapertools.cachePage(url)
+
+    # Extrae las entradas (carpetas)
+   
+    patron = '<div id="abecedario">(.*?)<div class="corte"></div>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    logger.info("hay %d matches" % len(matches))
+    
+    itemlist = []
+    for match in matches:
+        patron  = '<a href="(.*?)">(.*?)</a>'
+        matches2 = re.compile(patron,re.DOTALL).findall(match)
+        logger.info("hay %d matches2" % len(matches2))
+
+        for match2 in matches2:
+        # Atributos
+            scrapedurl = "http://pelispekes.com"+match2[0]
+            scrapedtitle =match2[1]
+            scrapedthumbnail = ""
+            scrapedplot = ""
+            if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+            
+            # Aï¿½ade al listado de XBMC
+            itemlist.append( Item(channel=item.channel , action="Generico"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
+        
+    return itemlist
+
