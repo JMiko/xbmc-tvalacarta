dcca518c1b54dfd4c612f9e28c5e8cc05f36992e
carlescarmonacalpe@gmail.com <carlescarmonacalpe@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-11-27 17:55:50 +0000

    canal actualizado

--- trunk/pelisalacarta/pelisalacarta/channels/delatv.py
+++ trunk/pelisalacarta/pelisalacarta/channels/delatv.py
@@ -1,7 +1,7 @@
 # -*- coding: iso-8859-1 -*-
 #------------------------------------------------------------
 # pelisalacarta - XBMC Plugin
-# Canal para delatv
+# Canal Descarregadirecta Carles Carmona
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 #------------------------------------------------------------
 import urlparse,urllib2,urllib,re
@@ -17,46 +17,169 @@ from servers import servertools
 CHANNELNAME = "delatv"
 DEBUG = True
 
+
 def isGeneric():
     return True
 
 
 def mainlist(item):
-    logger.info("[delatv.py] mainlist")
+    logger.info("[pelispekes.py] mainlist")
     itemlist=[]
 
-    itemlist.append( Item(channel=CHANNELNAME , action="listvideos"        , title="Ultimas Películas Añadidas"    , url="http://delatv.com/"))
-    itemlist.append( Item(channel=CHANNELNAME , action="ListaCat"          , title="Listado por Genero"            , url="http://delatv.com/"))
-    itemlist.append( Item(channel=CHANNELNAME , action="ListaAlfa"         , title="Listado Alfanumerico"          , url="http://delatv.com/" ))
+    itemlist.append( Item(channel=CHANNELNAME , action="Generico"        , title="Novedades"                      , url="http://delatv.com/"))
+    itemlist.append( Item(channel=CHANNELNAME , action="Categorias"        , title="Categorias"                      , url="http://delatv.com/"))
+    itemlist.append( Item(channel=CHANNELNAME , action="Abecedario"        , title="Abecedario"                      , url="http://delatv.com/"))
+    
 
     return itemlist
 
-def ListaCat(item):
-    logger.info("[delatv.py] ListaCat")
 
-    # Tiene el mismo HTML que cineadicto
-    import cineadicto
-    return cineadicto.ListaCat(item)
+def Generico(item):
+    logger.info("[filmixt.py] Generico")
 
-def ListaAlfa(item):
-    logger.info("[delatv.py] ListaAlfa")
+    url = item.url
+                
+    data = scrapertools.cachePage(url)
 
-    # Tiene el mismo HTML que cineadicto
-    import cineadicto
-    return cineadicto.ListaAlfa(item)
+    # Extrae las entradas (carpetas)
+   
+    patron = 'class="filmgal">(.*?)<strong>DuraciÃ³n: </strong>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    logger.info("hay %d matches" % len(matches))
+    
 
-def listvideos(item):
-    logger.info("[delatv.py] listvideos")
+    itemlist = []
+    for match in matches:
+        patron  = '<a target="_blank" href="(.*?)">.*?'
+        patron += '<img width="145" height="199" border="0" src="(.*?)" alt="(.*?)"/>.*?'
+        patron += '<strong>Sinopsis:</strong>(.*?)</div>'
+        matches2 = re.compile(patron,re.DOTALL).findall(match)
+        logger.info("hay %d matches2" % len(matches2))
 
-    # Tiene el mismo HTML que cineadicto
-    import cineadicto
-    return cineadicto.listvideos(item)
+        for match2 in matches2:
+        # Atributos
+            scrapedurl = match2[0]
+            scrapedtitle =match2[2].replace("Ver pelicula","").replace("&#8211;","").strip()
+            scrapedthumbnail = match2[1]
+            scrapedplot = match2[3]
+            if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+            
+            # Aï¿½ade al listado de XBMC
+            itemlist.append( Item(channel=item.channel , action="buscavideos"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
+    
+    # ------------------------------------------------------
+    # Extrae la p?gina siguiente
+    # ------------------------------------------------------
+    #patron = '<a href="([^"]+)" >\&raquo\;</a>'
+    patron  = "class='current'>[^<]+</span><a href='([^']+)'"
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    if DEBUG:
+        scrapertools.printMatches(matches)
+
+    for match in matches:
+        scrapedtitle = "!Pagina siguiente"
+        scrapedurl = match
+        scrapedthumbnail = ""
+        scrapeddescription = ""
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+
+        itemlist.append( Item(channel=item.channel , action="Generico"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
+    
+    
+    return itemlist
 
 def detail(item):
-    logger.info("[delatv.py] listvideos")
+    logger.info("[Descarregadirecta.py] detail")
 
-    #player(new Array('videozer', '8J18b3'))
+    title = item.title
+    thumbnail = item.thumbnail
+    plot = item.plot
+    scrapedurl = ""
+    url = item.url
 
-    # Tiene el mismo HTML que cineadicto
-    import cineadicto
-    return cineadicto.detail(item)
+    itemlist = []
+
+    # Descarga la pï¿½gina
+    data = scrapertools.cachePage(url)
+    
+    # Usa findvideos    
+    listavideos = servertools.findvideos(data)
+    
+    itemlist = []
+    
+    for video in listavideos:
+        server = video[2]
+        scrapedtitle = item.title + " [" + server + "]"
+        scrapedurl = video[1]
+        
+        itemlist.append( Item(channel=CHANNELNAME, action="play" , title=scrapedtitle , url=scrapedurl, thumbnail=item.thumbnail, plot=item.plot, server=server, folder=False))
+
+
+
+    return itemlist
+
+def Categorias(item):
+    logger.info("[filmixt.py] Categorias")
+
+    url = item.url
+                
+    data = scrapertools.cachePage(url)
+
+    # Extrae las entradas (carpetas)
+   
+    patron = '<div id="genero">(.*?)<div class="corte"></div>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    logger.info("hay %d matches" % len(matches))
+    
+    itemlist = []
+    for match in matches:
+        patron  = '<a href="(.*?)">(.*?)</a>'
+        matches2 = re.compile(patron,re.DOTALL).findall(match)
+        logger.info("hay %d matches2" % len(matches2))
+
+        for match2 in matches2:
+        # Atributos
+            scrapedurl = "http://delatv.com"+match2[0]
+            scrapedtitle =match2[1]
+            scrapedthumbnail = ""
+            scrapedplot = ""
+            if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+            
+            # Aï¿½ade al listado de XBMC
+            itemlist.append( Item(channel=item.channel , action="Generico"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
+        
+    return itemlist
+
+def Abecedario(item):
+    logger.info("[filmixt.py] Abecedario")
+
+    url = item.url
+                
+    data = scrapertools.cachePage(url)
+
+    # Extrae las entradas (carpetas)
+   
+    patron = '<div id="abecedario">(.*?)<div class="corte"></div>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    logger.info("hay %d matches" % len(matches))
+    
+    itemlist = []
+    for match in matches:
+        patron  = '<a href="(.*?)">(.*?)</a>'
+        matches2 = re.compile(patron,re.DOTALL).findall(match)
+        logger.info("hay %d matches2" % len(matches2))
+
+        for match2 in matches2:
+        # Atributos
+            scrapedurl = "http://delatv.com"+match2[0]
+            scrapedtitle =match2[1]
+            scrapedthumbnail = ""
+            scrapedplot = ""
+            if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+            
+            # Aï¿½ade al listado de XBMC
+            itemlist.append( Item(channel=item.channel , action="Generico"   , title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail, plot=scrapedplot ))
+        
+    return itemlist
+
+    
