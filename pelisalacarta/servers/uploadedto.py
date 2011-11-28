a5f782cc72b4c24540ea8c2a2e544f5f3ecb025c
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-11-28 17:09:24 +0000

    Añadidos conectores de nuevos servidores, sólo para detectar los patrones cuando usas Filenium

---
+++ pelisalacarta/servers/uploadedto.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Conector para uploaded.to
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#------------------------------------------------------------
+
+import urlparse,urllib2,urllib,re
+import os
+
+from core import scrapertools
+from core import logger
+from core import config
+
+def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
+    logger.info("[uploadedto.py] get_video_url(page_url='%s')" % page_url)
+    video_urls = []
+    return video_urls
+
+# Encuentra vídeos del servidor en el texto pasado
+def find_videos(data):
+    encontrados = set()
+    devuelve = []
+
+    # http://uploaded.to/file/1haty8nt
+    patronvideos  = '(http://uploaded.to/file/[a-zA-Z0-9]+)'
+    logger.info("[uploadedto.py] find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+
+    for match in matches:
+        titulo = "[uploaded.to]"
+        url = match
+        if url not in encontrados:
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'uploadedto' ] )
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada="+url)
+
+    return devuelve
