03b2f7419df20108780a582a4ec35570377eea61
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2011-12-12 01:59:11 +0000

    Versión 3.2.4 final

--- trunk/pelisalacarta/default.py
+++ trunk/pelisalacarta/default.py
@@ -9,8 +9,8 @@
 __plugin__  = "pelisalacarta"
 __author__  = "pelisalacarta"
 __url__     = "http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/"
-__date__    = "19 Octubre 2011"
-__version__ = "3.2.3"
+__date__    = "12 Diciembre 2011"
+__version__ = "3.2.4"
 
 import os
 import sys
