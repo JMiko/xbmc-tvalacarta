964d32b4d426f8c1a2508eee9a3462a51cf47827
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-01-29 18:07:06 +0000

    Mejora en los patrones de videobam a partir de un comentario en el blog (gracias :)

--- pelisalacarta/servers/videobam.py
+++ pelisalacarta/servers/videobam.py
@@ -67,5 +67,20 @@ def find_videos(data):
             encontrados.add(url)
         else:
             logger.info("  url duplicada="+url)
-                    
+
+    # http://videobam.com/fsgUt
+    patronvideos  = 'http://videobam.com/([\w]+)'
+    logger.info("[videobam.py] find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+
+    for match in matches:
+        titulo = "[videobam]"
+        url = "http://videobam.com/"+match
+        if url not in encontrados:
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'videobam' ] )
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada="+url)
+
     return devuelve
