70d9be0365beeb8589fd2c40f18c693174421319
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-05-16 23:34:39 +0000

    Conector para bayfiles free (aún no funciona...)

--- trunk/pelisalacarta/servers/bayfiles.py
+++ trunk/pelisalacarta/servers/bayfiles.py
@@ -11,65 +11,65 @@ import os
 from core import scrapertools
 from core import logger
 from core import config
-from core import unpackerjs
+
+def test_video_exists( page_url ):
+    logger.info("[bayfiles.py] test_video_exists(page_url='%s')" % page_url)
+
+    return True,""
 
 def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
-    logger.info("[bayfiles.py] url="+page_url)
+    logger.info("[bayfiles.py] get_video_url("+page_url+")")
     from servers import servertools
     video_urls = []
-    if config.get_setting("realdebridpremium")=="true" :
-        if "bayfiles" in servertools.REALDEBRID_SERVERS:
-            pass
-            
-    elif config.get_setting("fileniumpremium")=="true":
-        if "bayfiles" in servertools.FILENIUM_SERVERS:
-            pass
-    else:        
-        data = scrapertools.cache_page(page_url)
-        try:
-            vfid = re.compile('var vfid = ([^;]+);').findall(data)[0]
-        except:
-            logger.info("[bayfiles.py] Error no encontro vfid")
-            return ''
-        try:
-            delay = re.compile('var delay = ([^;]+);').findall(data)[0]
-            delay = int(delay)
-        except:
-            delay = 300
-        
-        from platformcode.xbmc import xbmctools
-     
 
-        t = millis()
-        #http://bayfiles.com/ajax_download?_=1330173386041&action=startTimer&vfid=918891
-        url_token = "http://bayfiles.com/ajax_download?_=%s&action=startTimer&vfid=%s"%(t,vfid)
-        data = scrapertools.cache_page(url_token)
-        datajson = load_json(data)
-        #print datajson
-        if datajson['set']==True:
-            token=datajson['token']
-            resultado = xbmctools.handle_wait(delay,"Progreso","Conectando con servidor BayFiles (Free)")
-            #if resultado == False:
-                
-            url_ajax = 'http://bayfiles.com/ajax_download'
-            post = "action=getLink&vfid=%s&token=%s" %(vfid,token)
-            data = scrapertools.cache_page( url_ajax , post=post, headers=[['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'],['Referer',page_url]] )
-        
-            # Extrae la url del video
-            patron = 'onclick="javascript:window.location.href = \'(.+?)\''
-            matches = re.compile(patron,re.DOTALL).findall(data)
-            #scrapertools.printMatches(matches)
+    data = scrapertools.cache_page(page_url)
+    try:
+        vfid = re.compile('var vfid = ([^;]+);').findall(data)[0]
+    except:
+        logger.info("[bayfiles.py] Error no encontro vfid")
+        return ''
+    try:
+        delay = re.compile('var delay = ([^;]+);').findall(data)[0]
+        delay = int(delay)
+    except:
+        delay = 300
+
+    logger.info("[bayfiles.py] vfid="+vfid)
+    logger.info("[bayfiles.py] delay="+str(delay))
+
+    from platformcode.xbmc import xbmctools
+ 
+    t = millis()
+    #http://bayfiles.com/ajax_download?_=1336330599281&action=startTimer&vfid=2174049
+    url_token = "http://bayfiles.com/ajax_download?_=%s&action=startTimer&vfid=%s"%(t,vfid)
+    data = scrapertools.cache_page(url_token)
+    logger.info("data="+data)
+    datajson = load_json(data)
+
+    if datajson['set']==True:
+        token=datajson['token']
+        resultado = xbmctools.handle_wait(delay,"Progreso","Conectando con servidor BayFiles (Free)")
+        #if resultado == False:
             
-            if len(matches)>0:
-                mediaurl = matches[0]
-                try:
-                    location = scrapertools.getLocationHeaderFromResponse(mediaurl)
-                    if location:
-                        mediaurl = location
-                except:
-                    logger.info("Error al redireccionar")
-                mediaurl = mediaurl + "|Referer="+urllib.quote(page_url)
-                video_urls.append( ["."+mediaurl.rsplit('.',1)[1]+" [bayfiles]",mediaurl])
+        url_ajax = 'http://bayfiles.com/ajax_download'
+        post = "action=getLink&vfid=%s&token=%s" %(vfid,token)
+        data = scrapertools.cache_page( url_ajax , post=post, headers=[['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'],['Referer',page_url]] )
+    
+        # Extrae la url del video
+        patron = 'onclick="javascript:window.location.href = \'(.+?)\''
+        matches = re.compile(patron,re.DOTALL).findall(data)
+        #scrapertools.printMatches(matches)
+        
+        if len(matches)>0:
+            mediaurl = matches[0]
+            try:
+                location = scrapertools.getLocationHeaderFromResponse(mediaurl)
+                if location:
+                    mediaurl = location
+            except:
+                logger.info("Error al redireccionar")
+            mediaurl = mediaurl + "|Referer="+urllib.quote(page_url)
+            video_urls.append( ["."+mediaurl.rsplit('.',1)[1]+" [bayfiles]",mediaurl,60])
 
     for video_url in video_urls:
         logger.info("[bayfiles.py] %s - %s" % (video_url[0],video_url[1]))
