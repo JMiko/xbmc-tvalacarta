007d3a0f7833a5a7e03b33e1f478bc6f2924a335
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-05-16 23:33:56 +0000

    Vidxden (sólo con descarga en segundo plano, muy experimental todavía)

--- trunk/pelisalacarta/servers/vidxden.py
+++ trunk/pelisalacarta/servers/vidxden.py
@@ -16,35 +16,24 @@ from core import unpackerjs
 def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
 
     logger.info("[vidxden.py] url="+page_url)
-    if ".html" not in page_url:
-        logger.info("[vidxden.py] URL incompleta")
-        data = scrapertools.cache_page(page_url)
-        patron = '<input name="fname" type="hidden" value="([^"]+)">'
-        matches = re.compile(patron,re.DOTALL).findall(data)
-        page_url = page_url+"/"+matches[0]+".html"
-        logger.info("[vidxden.py] url="+page_url)
-        
-    # Lo pide una vez
-    scrapertools.cache_page( page_url , headers=[['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14']] )
-    
-    # Lo pide una segunda vez, como si hubieras hecho click en el banner
-    patron = 'http\:\/\/www\.vidxden\.com/([^\/]+)/(.*?)\.html'
-    matches = re.compile(patron,re.DOTALL).findall(page_url)
-    logger.info("[vidxden.py] fragmentos de la URL")
-    scrapertools.printMatches(matches)
-    
-    codigo = ""
-    nombre = ""
-    if len(matches)>0:
-        codigo = matches[0][0]
-        nombre = matches[0][1]
 
-    #op=download1&usr_login=&id=kmxlztxqio5a&fname=No_Estas_Sola__2007__DVDRip_.avi&referer=&method_free=Continue+to+Video
-    post = "op=download1&usr_login=&id="+codigo+"&fname="+nombre+"&referer=&method_free=Continue+to+Video"
-    data = scrapertools.cache_page( page_url , post=post, headers=[['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'],['Referer',page_url]] )
+    # Lo pide una vez
+    headers = []
+    headers.append(['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'])
+    data = scrapertools.cache_page( page_url , headers=headers )
+    fname = scrapertools.get_match(data,'<input name="fname" type="hidden" value="([^"]+)">')
+    codigo = scrapertools.get_match(page_url,'vidxden.com/(\w+)')
+
+    # Lo pide una segunda vez, como si hubieras hecho click en el banner
+    #op=download1&usr_login=&id=qtrv0ufkz3e4&fname=El_cazador_de_sue_os-dvd.avi&referer=&method_free=Continue+to+Video
+    headers.append(['Referer',page_url])
+    post = "op=download1&usr_login=&id="+codigo+"&fname="+fname+"&referer=&method_free=Continue+to+Video"
+    data = scrapertools.cache_page( page_url , post=post, headers=headers )
+    logger.info("data="+data)
 
     # Extrae el trozo cifrado
-    patron = '<div id="embedcontmvshre"[^>]+>(.*?)</div>'
+    #<div id="embedcontmvshre" style="position: absolute; top: 0; left: 0; visibility: hidden;"><script type='text/javascript'>eval(function(p,a,c,k,e,d){while(c--)if(k[c])p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c]);return p}('1i.1h(\'<8 10="1g"1f="1e:1d-1c-1b-1a-19"q="p"o="n"18="3://b.7.4/a/17.16"><2 1="u"0="t"/><2 1="s"0="r"/><2 1="m"0="3://i/l/6.k"/><2 1="f"0="5"><2 1="g"0="5"/><2 1="e"0="c"/><2 1="j"0="h"/><2 1="z"0="3://y.x.4:w/d/v/6"/><9 10="15"14="13/7"z="3://y.x.4:w/d/v/6"u="t"s="r"q="p"o="n"m="3://i/l/6.k"j="h"g="5"f="5"e="c"12="3://b.7.4/a/11/"></9></8>\');',36,55,'value|name|param|http|com|false|qtrv0ufkz3e4|divx|object|embed|plugin|go|Play||previewMessage|allowContextMenu|bannerEnabled|true||autoPlay|jpg|00249|previewImage|318|height|640|width|transparent|wmode|Stage6|custommode|opujxvaorizu2mdg6fst2fjdzlrn4p437h3lsbz5fjkxs|364|divxden|s31|src|id|download|pluginspage|video|type|np_vid|cab|DivXBrowserPlugin|codebase|CC0F21721616|9C46|41fa|D0AB|67DABFBF|clsid|classid|ie_vid|write|document'.split('|')))</script></div>
+    patron = "(<script type='text/javascript'>eval\(function.*?</script>)"
     matches = re.compile(patron,re.DOTALL).findall(data)
     #scrapertools.printMatches(matches)
     data = ""
@@ -72,9 +61,9 @@ def get_video_url( page_url , premium = False , user="" , password="", video_pas
         scrapertools.printMatches(matches)
     
     video_urls = []
-    
+
     if len(matches)>0:
-        video_urls.append( ["."+matches[0].rsplit('.',1)[1]+" [vidxden]",matches[0]+"|Referer="+page_url])
+        video_urls.append( ["[vidxden]",matches[0]+"|Referer="+urllib.quote(page_url)+"&User-Agent="+urllib.quote('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')])
 
     for video_url in video_urls:
         logger.info("[vidxden.py] %s - %s" % (video_url[0],video_url[1]))
@@ -86,29 +75,14 @@ def find_videos(text):
     encontrados = set()
     devuelve = []
 
-    # http://www.vidxden.com/3360qika02mo/whale.wars.s04e10.hdtv.xvid-momentum.avi.html
-    patronvideos  = '(http://www.vidxden.com/[A-Z0-9a-z]+/.*?html)'
-    logger.info("[vidxden.py] find_videos #"+patronvideos+"#")
-    matches = re.compile(patronvideos,re.DOTALL).findall(text)
-
-    for match in matches:
-        titulo = "[vidxden]"
-        url = match
-        if url not in encontrados:
-            logger.info("  url="+url)
-            devuelve.append( [ titulo , url , 'vidxden' ] )
-            encontrados.add(url)
-        else:
-            logger.info("  url duplicada="+url)
-            
     # http://www.vidxden.com/qya0qmf3k502
-    patronvideos  = 'http://www.vidxden.com/([\w]+)'
+    patronvideos  = '(vidxden.com/\w+)'
     logger.info("[vidxden.py] find_videos #"+patronvideos+"#")
     matches = re.compile(patronvideos,re.DOTALL).findall(text)
 
     for match in matches:
         titulo = "[vidxden]"
-        url = "http://www.vidxden.com/"+match
+        url = "http://www."+match
         if url not in encontrados:
             logger.info("  url="+url)
             devuelve.append( [ titulo , url , 'vidxden' ] )
