07c8974bf734ba696ab86d847efaed512d5181a7
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-05-28 01:28:10 +0000

    Nuevos conectores RD/Filenium: filereactor, shareflare, 180upload, vidhog, rapidgator, 1fichier, fiberupload, cramit

---
+++ trunk/pelisalacarta/servers/one80upload.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Conector para 180upload
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#------------------------------------------------------------
+
+import urlparse,urllib2,urllib,re
+import os
+
+from core import scrapertools
+from core import logger
+from core import config
+
+def test_video_exists( page_url ):
+    return True,""
+
+def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
+    logger.info("[one80upload.py] get_video_url(page_url='%s')" % page_url)
+    video_urls = []
+    return video_urls
+
+# Encuentra vídeos del servidor en el texto pasado
+def find_videos(data):
+    encontrados = set()
+    devuelve = []
+
+    #http://180upload.com/rtp8y30mlfj0
+    patronvideos  = '(180upload.com/[a-z0-9]+)'
+    logger.info("[one80upload.py] find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+
+    for match in matches:
+        titulo = "[180upload]"
+        url = "http://"+match
+        if url not in encontrados:
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'one80upload' ] )
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada="+url)
+
+    return devuelve
