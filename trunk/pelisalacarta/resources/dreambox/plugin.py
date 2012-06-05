953c19aa0bc7dfc8171d42080543c6b3cdfcfd97
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-06-05 10:12:32 +0000

    Versión 3.2.9

--- trunk/pelisalacarta/resources/dreambox/plugin.py
+++ trunk/pelisalacarta/resources/dreambox/plugin.py
@@ -1579,5 +1579,5 @@ def main(session, **kwargs):
 def Plugins(**kwargs):
     print "[pelisalacarta] Plugins"
     return [
-        PluginDescriptor(name = "pelisalacarta", description = "pelisalacarta 3.2.8 para Dreambox", icon="plugin-icon.png", where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main),
-        PluginDescriptor(name = "pelisalacarta", description = "pelisalacarta 3.2.8 para Dreambox", icon="plugin-icon.png", where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main)]
+        PluginDescriptor(name = "pelisalacarta", description = "pelisalacarta 3.2.9 para Dreambox", icon="plugin-icon.png", where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main),
+        PluginDescriptor(name = "pelisalacarta", description = "pelisalacarta 3.2.9 para Dreambox", icon="plugin-icon.png", where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main)]
