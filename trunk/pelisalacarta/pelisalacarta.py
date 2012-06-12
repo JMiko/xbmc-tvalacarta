b58944a0a295146e9a0c0059415398059c09f9b4
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-06-12 00:59:46 +0000

    Cambios para mediaserver

--- trunk/pelisalacarta/pelisalacarta.py
+++ trunk/pelisalacarta/pelisalacarta.py
@@ -46,22 +46,55 @@ class Handler(BaseHTTPRequestHandler):
         logger.info(" HOST: "+host)
         logger.info("-----------------------------------------------------------------")
 
-        if self.path.startswith("/"+PLATFORM_NAME):
+        if self.path.startswith("/mediaserver/resources/images") and self.path.endswith(".jpg"):
+            f=open( os.path.join ( config.get_runtime_path() , self.path.replace("/mediaserver/","") ) )
+            self.send_response(200)
+            self.send_header('Content-type', 'image/jpeg')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+
+        elif self.path.startswith("/mediaserver/resources/images") and self.path.endswith(".png"):
+            f=open( os.path.join ( config.get_runtime_path() , self.path.replace("/mediaserver/","") ) )
+            self.send_response(200)
+            self.send_header('Content-type', 'image/png')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+
+        elif self.path.startswith("/css"):
+            f=open( os.path.join ( config.get_runtime_path() , "platformcode" , "mediaserver" , "template" , self.path[1:] ) )
+            self.send_response(200)
+            self.send_header('Content-type', 'text/css')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+
+        elif self.path.startswith("/js"):
+            f=open( os.path.join ( config.get_runtime_path() , "platformcode" , "mediaserver" , "template" , self.path[1:] ) )
+            self.send_response(200)
+            self.send_header('Content-type', 'text/js')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+
+        elif self.path.endswith(".html"):
+            f=open( os.path.join ( config.get_runtime_path() , "platformcode" , "mediaserver" , "template" , self.path[1:] ) )
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+
+        elif self.path.startswith("/"+PLATFORM_NAME):
             exec "from platformcode."+PLATFORM_NAME+" import launcher"
             respuesta = launcher.controller(plugin_name=PLUGIN_NAME,port=PORT,host=host,path=self.path,headers=self.headers)
-        else:
-            respuesta = ""
 
-        self.send_response(200)
-        
-        if PLATFORM_NAME == "rss":
-            self.send_header('Content-Type', 'text/xml')
-        else:
-            self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(respuesta)
-        self.wfile.close()
-        logger.info("-----------------------------------------------------------------")
+            self.send_response(200)
+            self.send_header('Content-Type', launcher.get_content_type())
+            self.end_headers()
+            self.wfile.write(respuesta)
+            self.wfile.close()
 
         return
     
@@ -89,6 +122,8 @@ for fichero in os.listdir( cachedir ):
     os.remove( os.path.join( cachedir , fichero ) )
 
 try:
+    # FIXME: Mejora multithread - http://blog.doughellmann.com/2007/12/pymotw-basehttpserver.html - http://stackoverflow.com/questions/2398144/python-basehttpserver-httpserver-concurrency-threading)
+    
     print "Iniciando el servidor en http://"+myip+":"+str(PORT) 
     print "La URL para "+PLATFORM_NAME+" es http://"+myip+":"+str(PORT)+"/"+PLATFORM_NAME
     # Levanta el servidor
@@ -107,10 +142,11 @@ try:
     server.serve_forever()
 except KeyboardInterrupt:
     print '^C received, shutting down server'
+    # FIXME: Aún así a veces no se muere. Probar con esto http://stackoverflow.com/questions/10613194/how-to-deal-with-python-basehttpserver-killed-but-the-port-is-still-be-occupied
     server.socket.close()
     server.shutdown()
     import sys
     sys.exit()
     raise SystemExit
     os._exit(0)
-    print "Sigo aqui"
+    print "Sigo aqui" 
