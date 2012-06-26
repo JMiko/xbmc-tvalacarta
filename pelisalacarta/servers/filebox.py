06f4d27b1574cb492a71855e683317abf933f584
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-06-26 00:22:13 +0000

    Arreglado un fallo en filebox

--- pelisalacarta/servers/filebox.py
+++ pelisalacarta/servers/filebox.py
@@ -23,25 +23,29 @@ def get_video_url( page_url , premium = False , user="" , password="", video_pas
     '''
     logger.info("[filebox.py] URL ")
     data = scrapertools.cache_page(page_url)
+    # Espera los 5 segundos
+    if "xbmc" in config.get_platform():
+        from platformcode.xbmc import xbmctools
+        xbmctools.handle_wait(5,"filebox",'')
+    else:
+        import time
+        time.sleep(5)
+
+
     patron  = '<input type="hidden" name="id" value="([^"]+)">[^<]+'
     patron += '<input type="hidden" name="rand" value="([^"]+)">'
     matches = re.compile(patron,re.DOTALL).findall(data)
 
-    # Lo pide una vez
-    #scrapertools.cache_page( page_url , headers=[['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14']] )
-    from platformcode.xbmc import xbmctools
-    xbmctools.handle_wait(5,"filebox",'')
-    # Lo pide una segunda vez, como si hubieras hecho click en el banner
-    
     codigo = ""
     nombre = ""
     if len(matches)>0:
         codigo = matches[0][0]
         rand = matches[0][1]
 
-    post = "op=download2&id="+codigo+"&rand="+rand+"&referer="+page_url+"&method_free=&method_premium=&down_direct=1"
+    post = "op=download2&id="+codigo+"&rand="+rand+"&referer=&method_free=&method_premium=&down_direct=1"
+
     data = scrapertools.cache_page( page_url , post=post, headers=[['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'],['Referer',page_url]] )
-    print data
+    logger.info("data="+data)
     # Busca el video online o archivo de descarga 
     patron = 'href="([^"]+)">>>> Download File <<<<'
     matches = re.compile(patron,re.DOTALL).findall(data)
@@ -68,15 +72,30 @@ def find_videos(data):
     encontrados = set()
     devuelve = []
 
-    # http://www.filebox.com/729x1eo9zrx1
-    patronvideos  = '(http://www.filebox.com/[0-9a-zA-Z]+)'
+    # http://www.filebox.com/embed-wa5p8wzh7tlq-700x385.html
+    patronvideos  = 'filebox.com/embed-([0-9a-zA-Z]+)'
     logger.info("[filebox.py] find_videos #"+patronvideos+"#")
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[filebox]"
-        url = match
+        url = "http://www.filebox.com/"+match
         if url not in encontrados:
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'filebox' ] )
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada="+url)
+
+    # http://www.filebox.com/729x1eo9zrx1
+    patronvideos  = 'filebox.com/([0-9a-zA-Z]+)'
+    logger.info("[filebox.py] find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+
+    for match in matches:
+        titulo = "[filebox]"
+        url = "http://www.filebox.com/"+match
+        if url!="http://www.filebox.com/embed" and url not in encontrados:
             logger.info("  url="+url)
             devuelve.append( [ titulo , url , 'filebox' ] )
             encontrados.add(url)
