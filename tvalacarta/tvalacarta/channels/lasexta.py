71e97e10efe8a18e1ef803094ab3bc367ddf1bb6
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-08-09 01:16:09 +0000

    Actualizado la sexta para que use la versión de la web. Menos calidad, pero muchos más vídeos.

--- tvalacarta/tvalacarta/channels/lasexta.py
+++ tvalacarta/tvalacarta/channels/lasexta.py
@@ -2,7 +2,7 @@
 #------------------------------------------------------------
 # Canal para la sexta
 #------------------------------------------------------------
-import urlparse,re
+import urlparse,re,urllib
 
 from core import logger
 from core import scrapertools
@@ -12,7 +12,7 @@ logger.info("[sexta.py] init")
 
 DEBUG = True
 CHANNELNAME = "lasexta"
-MAIN_URL = "http://www.lasexta.com/lg/programas"
+
 def isGeneric():
     return True
 
@@ -20,116 +20,215 @@ def mainlist(item):
     logger.info("[sexta.py] mainlist")
 
     # Descarga la pagina
-    item.url = MAIN_URL
-    return programas(item)
+    url = "http://www.lasexta.com/sextatv"
+    data = scrapertools.cachePage(url)
 
-def programas(item):
-    logger.info("[sexta.py] programas")
     itemlist = []
-
-    # Descarga la página
-    data = scrapertools.cache_page(item.url)
-    logger.info(data)
-
-    # Extrae las series
-    patron  = '<div class="item">[^<]+'
-    patron += '<a href="([^"]+)" title="([^"]+)"[^<]+'
-    patron += '<!-- <[^<]+'
-    patron += '<img src="([^"]+)"'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    if DEBUG: scrapertools.printMatches(matches)
-    
-    for scrapedurl,scrapedtitle,scrapedthumbnail in matches:
-        scrapedurl = urlparse.urljoin(item.url,scrapedurl)
-        itemlist.append( Item(channel=item.channel, title=scrapedtitle , action="episodios" , url=urlparse.urljoin(item.url,scrapedurl), thumbnail=scrapedthumbnail , show = scrapedtitle, folder=True) )
+    salir = False
+    pagina = 0
+    while not salir:
+        pageitems = getprogramaspagina("http://www.lasexta.com/sextatv/reload_programs","item_id=1&show_id=1&bd_id=1&pagina=%d&limit=3" % pagina)
+        itemlist.extend( pageitems )
+        pagina = pagina + 15
+        salir = len(pageitems)<15
     
     return itemlist
 
-def episodios(item):
-    logger.info("[sexta.py] episodios")
-    itemlist = []
-
-    # Descarga la página
-    data = scrapertools.cache_page(item.url)
+def getprogramaspagina(url,post):
+    logger.info("[sexta.py] getprogramaspagina post="+post)
+    
+    headers = [
+            ['User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'],
+            ['X-Requested-With','XMLHttpRequest'],
+            ['X-Prototype-Version','1.6.0.3'],
+            ['Referer','http://www.lasexta.com/sextatv']
+            ]
+    
+    # Esta peticion no se puede cachear
+    data = scrapertools.cachePage(url,post,headers)
     #logger.info(data)
-    
+
+    # Extrae las entradas
     '''
-    <div class="item">
-    <a href="/lg/programa_detalle/598163" title="¿Quién vive ahí? Una casa de fantasía es como vivir dentro de un cuento" id="item1" style="nav-index:1;nav-right:#item2;nav-left:#item0;" class="item_img_link nav_item">
-    <!-- <img class="media" src="media/img/1pxtrans.gif" width="293" height="165" alt="nombre de la fotografia" style="background:#0000C8"/> -->
-    <img class="media" src="http://www.sitios.lasexta.com/pictures/6673/396673/Sitios2010_20120331_1958598043_crop75.jpg" width="293" height="165" alt="¿Quién Vive Ahí?" style="background:#0000C8"/>
-    
-    <label class="item_data">
-    <b class="titulo" title="02-04-2012 ¿Quién vive ahí? Una casa de fantasía es como vivir dentro de un cuento">02-04-2012 ¿Quién vive ahí? Una casa de fantasía es como vivir dentro&#8230;</b>
-    </label>
+    <div class="capaseccionl item_vip">
+    <div class="player">
+    <a href="http://www.lasexta.com/sextatv/seloquehicisteis">
+    <img src="http://www.lasexta.com/media/sextatv/img/sextatv_logo_slqh.jpg" width="230" height="129" title="VÃ­deos de SÃ© lo que HicÃ­steis" alt="logotipo de SÃ© lo que hicÃ­steis" />
+    <label class="item_vip_player_label">SÃ© lo que HicÃ­steis</label>
+    <img src="http://www.lasexta.com/media/common/img/1pxtrans.gif" class="item_vip_player_link" alt="Ir a videos de SÃ© lo que HicÃ­steis"/>
     </a>
     </div>
     '''
-
-    # Extrae las series
-    patron  = '<div class="item">[^<]+'
-    patron += '<a href="([^"]+)" title="([^"]+)"[^<]+'
-    patron += '<!-- <[^<]+'
-    patron += '<img class="media" src="([^"]+)"[^<]+'
-    patron += '<label class="item_data">[^<]+'
-    patron += '<b class="titulo" title="([^"]+)">'
+    patron  = '<div class="capaseccionl item_vip">[^<]+'
+    patron += '<div class="player">[^<]+'
+    patron += '<a href="([^"]+)">[^<]+'
+    patron += '<img src="([^"]+)"[^>]+>[^<]+'
+    patron += '<label class="item_vip_player_label">([^<]+)</label>'
     matches = re.compile(patron,re.DOTALL).findall(data)
-    if DEBUG: scrapertools.printMatches(matches)
-    
-    for scrapedurl,showtitle,scrapedthumbnail,title in matches:
-        scrapedtitle = title
-        scrapedurl = urlparse.urljoin(item.url,scrapedurl)
-        itemlist.append( Item(channel=item.channel, title=scrapedtitle , action="partes" , url=urlparse.urljoin(item.url,scrapedurl), thumbnail=scrapedthumbnail , show = scrapedtitle, folder=True) )
-    
+    #if DEBUG: scrapertools.printMatches(matches)
 
-    '''
-    <div class="item">
-    <a href="/lg/programa_detalle/588133" title="Una casa de cine y la casa de la propia Barbie" id="item_s1" style="nav-index:1;nav-right:#item_s2;nav-left:#item_s0;" class="item_img_link nav_item_s">
-    <!-- <img src="media/img/1pxtrans.gif" width="186" height="105" alt="nombre de la fotografia" style="background:#000000" class="media" /> -->
-    <img src="http://www.sitios.lasexta.com/pictures/8713/388713/pictures_20120305_0958388713_crop75sub1.jpg" width="186" height="105" alt="¿Quién Vive Ahí?" style="background:#000000" class="media" />					<label class="item_data">
-    <b class="titulo" title="Una casa de cine y la casa de la propia Barbie" title-short="">Una casa de cine y la casa de la propia Barbie</b>
-    </label>
-    </a>
-    </div>
-    '''
+    itemlist = []
+    for match in matches:
+        scrapedtitle = match[2]
+        scrapedtitle = scrapertools.entityunescape(scrapedtitle)
+        scrapedurl = urlparse.urljoin(url,match[0])
+        scrapedthumbnail = urlparse.urljoin(url,match[1])
+        scrapedplot = ""
+        scrapedshow = scrapedtitle
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
 
-    # Extrae las series
-    patron  = '<div class="item">[^<]+'
-    patron += '<a href="([^"]+)" title="([^"]+)"[^<]+'
-    patron += '<!-- <[^<]+'
-    patron += '<img src="([^"]+)"[^<]+'
-    patron += '<label class="item_data">[^<]+'
-    patron += '<b class="titulo" title="([^"]+)"'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    if DEBUG: scrapertools.printMatches(matches)
-    
-    for scrapedurl,showtitle,scrapedthumbnail,title in matches:
-        scrapedtitle = title
-        scrapedurl = urlparse.urljoin(item.url,scrapedurl)
-        itemlist.append( Item(channel=item.channel, title=scrapedtitle , action="partes" , url=urlparse.urljoin(item.url,scrapedurl), thumbnail=scrapedthumbnail , show = scrapedtitle, folder=True) )
+        # AÃ±ade al listado
+        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="secciones" , url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot , show=scrapedshow , folder=True) )
 
     return itemlist
 
-def partes(item):
-    logger.info("[sexta.py] partes")
-    itemlist = []
-
-    # Descarga la página
-    data = scrapertools.cache_page(item.url)
-    logger.info(data)
+def secciones(item):
+    logger.info("[sexta.py] secciones")
     
-    # var video1 = new PlaylistItem(options = {url: 'http://videos.lasexta.com/quienviveahi/hd/PPD0001293004401_--QUI-_N_VIVE_AH-___-_QUI-_N_VIVE_AH-__18_03_2012_23_40_56_h264.mp4'});
-    patron = "url\:\s+'(http\://[a-z]+.lasexta.com/[^']+)'"
+    # Descarga la pagina
+    data = scrapertools.cachePage(item.url)
+    #logger.info(data)
+
+    # Ahora extrae las pestaÃ±as
+    patron  = '<li class="lsv_pestanas_li"><a id="[^"]+" href="javascript.change_videos.\'([^\']+)\',\'([^\']+)\'.."[^>]+><b class="left"></b>([^<]+)<b class="right"></b></a></li>'
+
     matches = re.compile(patron,re.DOTALL).findall(data)
     if DEBUG: scrapertools.printMatches(matches)
-    logger.info("matches="+str(matches))
+
+    itemlist = []
+
+    for match in matches:
+        scrapedurl = "http://www.lasexta.com/sextatv/change_videos/"+match[0]+"/"+match[1]
+        scrapedtitle = match[2]
+        # AÃ±ade al listado
+        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="videos" , url=scrapedurl, show=item.show , folder=True) )
+
+    return itemlist
+
+def videos(item):
+    logger.info("[sexta.py] videos")
+    
+    # Descarga la pagina
+    data = scrapertools.cachePage(item.url)
+    itemlist = parsevideos(item,data)
+
+    # Paginacion
+    # El id es 11_ultimos_15
+    # La pagina es un POST a http://www.lasexta.com/sextatv/reload con "seccion=11&pagina=15&tipo=ultimos&section_id"
+    while True:
+        patron = '<a href="javascript.reload.\'([^\']+)\'.;" class="siguiente" title="P&aacute;gina siguiente">P&aacute;gina siguiente</a>'
+        matches = re.compile(patron,re.DOTALL).findall(data)
+        if DEBUG: scrapertools.printMatches(matches)
+    
+        if len(matches)>0:
+            partes = matches[0].split("_")
+            url = "http://www.lasexta.com/sextatv/reload"
+            post = "seccion=%s&pagina=%s&tipo=%s&section_id" % (partes[0],partes[2],partes[1])
+            data = scrapertools.cachePage(url,post)
+            newitems = parsevideos(item,data)
+            itemlist.extend(newitems)
+        else:
+            break
+
+    # Caso especial "Ver todos los videos"
+    patron = '<a href="javascript.reload.\'([^\']+)\'.;">Ver todos'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    if len(matches)==0:
+        return itemlist
+    
+    partes = matches[0].split("_")
+    url = "http://www.lasexta.com/sextatv/reload"
+    post = "seccion=%s&pagina=%s&tipo=%s&section_id=%s" % (partes[0],partes[2],partes[1],partes[3])
+    data = scrapertools.cachePage(url,post)
+    
+    # Paginacion
+    # El id es 11_ultimos_15_1234
+    # La pagina es un POST a http://www.lasexta.com/sextatv/reload con "seccion=11&pagina=15&tipo=ultimos&section_id=1234"
+    while True:
+        patron = '<a href="javascript.reload.\'([^\']+)\'.;" class="siguiente" title="P&aacute;gina siguiente">P&aacute;gina siguiente</a>'
+        matches = re.compile(patron,re.DOTALL).findall(data)
+        if DEBUG: scrapertools.printMatches(matches)
+    
+        if len(matches)>0:
+            partes = matches[0].split("_")
+            url = "http://www.lasexta.com/sextatv/reload"
+            post = "seccion=%s&pagina=%s&tipo=%s&section_id=%s" % (partes[0],partes[2],partes[1],partes[3])
+            data = scrapertools.cachePage(url,post)
+            newitems = parsevideos(item,data)
+            itemlist.extend(newitems)
+        else:
+            break
+
+
+    return itemlist
+
+def parsevideos(item,data):
+    logger.info("[sexta.py] parsevideos")
+    #print data
+    
+    # Extrae los videos
+    '''
+    <div class="capaseccionl item">
+    <div class="player_programas">
+    <a href="http://www.lasexta.com/sextatv/quienviveahi/completos/quien_vive_ahi___jueves__21_de_octubre/315352/1"><img src="http://www.sitios.lasexta.com/pictures/215481/pictures_20101020_1601215481_crop1.jpg" width="170" height="127" title="quien_vive_ahi___jueves__21_de_octubre" alt="quien_vive_ahi___jueves__21_de_octubre" /></a>
+    <a href="http://www.lasexta.com/sextatv/quienviveahi/completos/quien_vive_ahi___jueves__21_de_octubre/315352/1" class="item_cortina">
+    <img src="http://www.lasexta.com/media/common/img/1pxtrans.gif" width="170" height="127" title="quien_vive_ahi___jueves__21_de_octubre" alt="quien_vive_ahi___jueves__21_de_octubre" />
+    <label class="item_cortina_text">El programa visit del campo o un&#8230;</label>
+    <label class="item_cortina_play">PLAY</label>
+    </a>
+    </div>
+    <h6 class="fecha">22/10/2010 </h6>
+    <h5 class="titulo"><a href="http://www.lasexta.com/sextatv/quienviveahi/completos/quien_vive_ahi___jueves__21_de_octubre/315352/1" title="quien_vive_ahi___jueves__21_de_octubre">aaaaaJueves, 21 de octubre</a></h5>
+    </div>
+    '''
+    patron  = '<div class="capaseccionl item">[^<]+'
+    patron += '<div class="player_programas">[^<]+'
+    patron += '<a href="([^"]+)"><img src="([^"]+)"[^>]+></a>.*?'
+    patron += '<label class="item_cortina_text">([^<]*)</label>.*?'
+    patron += '<h6 class="fecha">([^<]+)</h6>[^<]+'
+    patron += '<h5 class="titulo"><a[^>]+>([^<]+)</a>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    if DEBUG: scrapertools.printMatches(matches)
+    
+    itemlist = []
+    
+    for match in matches:
+        scrapedtitle = match[4]+" "+match[3]
+        scrapedurl = match[0]
+        scrapedthumbnail = match[1]
+        scrapedplot = match[2] 
+        scrapedpage = scrapedurl
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+
+        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="getVideos" , url=scrapedurl, page=scrapedpage, thumbnail=scrapedthumbnail, plot=scrapedplot , show=item.show , folder=True) )
+    
+    return itemlist
+
+def getVideos(item):
+    logger.info("[lasexta.py] getVideos")
+    itemlist = []
+    post = urllib.urlencode({"web":item.url})
+
+    headers = []
+    headers.append( [ "User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:14.0) Gecko/20100101 Firefox/14.0.1" ] )
+
+    location = scrapertools.get_header_from_response("http://www.descargavideos.tk/",post=post,header_to_get="location",headers=headers)
+    
+    headers = []
+    headers.append( [ "Referer","http://www.descargavideos.tk/" ] )
+    headers.append( [ "User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:14.0) Gecko/20100101 Firefox/14.0.1" ] )
+
+    data = scrapertools.cache_page(location)
+    logger.info("data="+data)
+    patron = '<a id=resenl[^>]+>([^<]+)<'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+
     i=1
-    for url in matches:
-        scrapedtitle = "("+str(i)+") "+item.title
-        scrapedurl = url
-        scrapedthumbnail = item.thumbnail
-        scrapedplot = item.plot
-        itemlist.append( Item(channel=item.channel, title=scrapedtitle , action="play" , url=scrapedurl, thumbnail=scrapedthumbnail , show = scrapedtitle, folder=False) )
+    for scrapedurl in matches:
+        url = scrapedurl
+        #scrapedVideoUrl = scrapedVideoUrl.replace("mp4:", " playpath=mp4:")
+        #scrapedVideoUrl = scrapedVideoUrl + " swfvfy=true swfurl=http://www.lasexta.com/media/swf/reproductor_sextatv/player_overlay.swf"
+        itemlist.append( Item(channel=CHANNELNAME, title="Parte %d"%i , action="play", server="Directo" , url=url, thumbnail=item.thumbnail, plot=item.plot, folder=False) )
         i=i+1
 
     return itemlist
