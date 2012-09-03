d99a4b8fbff580917c3ea6436021338b18818c34
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-09-03 22:44:42 +0000

    Ahora el refresco automático de la biblioteca al entrar a XBMC Eden es configurable

--- trunk/pelisalacarta/library_service.py
+++ trunk/pelisalacarta/library_service.py
@@ -22,7 +22,7 @@ from pelisalacarta.channels import cuevana
 import xbmcgui
 
 #Eliminar carpeta antes de actualizar
-
+    
 directorio = os.path.join(config.get_library_path(),"SERIES")
 logger.info ("directorio="+directorio)
 import shutil
@@ -36,35 +36,39 @@ if not os.path.exists(directorio):
 nombre_fichero_config_canal = os.path.join( config.get_data_path() , "series.xml" )
 
 try:
-    config_canal = open( nombre_fichero_config_canal , "r" )
-    
-    for serie in config_canal.readlines():
-        logger.info("[library_service.py] serie="+serie)
-        serie = serie.split(",")
-    
-        ruta = os.path.join( config.get_library_path() , "SERIES" , serie[0] )
-        logger.info("[library_service.py] ruta =#"+ruta+"#")
-        if os.path.exists( ruta ):
-            logger.info("[library_service.py] Actualizando "+serie[0])
-            item = Item(url=serie[1], show=serie[0])
-            try:
-                if serie[2].strip()=='seriesyonkis': itemlist = seriesyonkis.episodios(item)
-                if serie[2].strip()=='cuevana': itemlist = cuevana.episodios(item)
-            except:
-                itemlist = []
-        else:
-            logger.info("[library_service.py] No actualiza "+serie[0]+" (no existe el directorio)")
-            itemlist=[]
-            
-        i=0
-        for item in itemlist:
-            i = i + 1
-            item.show=serie[0].strip()
-            if i<len(itemlist):
-                library.savelibrary( titulo=item.title , url=item.url , thumbnail=item.thumbnail , server=item.server , plot=item.plot , canal=item.channel , category="Series" , Serie=item.show , verbose=False, accion="play_from_library", pedirnombre=False, subtitle=item.subtitle )
-    
-    import xbmc
-    xbmc.executebuiltin('UpdateLibrary(video)')
+
+    if config.get_setting("updatelibrary")=="true":
+        config_canal = open( nombre_fichero_config_canal , "r" )
+        
+        for serie in config_canal.readlines():
+            logger.info("[library_service.py] serie="+serie)
+            serie = serie.split(",")
+        
+            ruta = os.path.join( config.get_library_path() , "SERIES" , serie[0] )
+            logger.info("[library_service.py] ruta =#"+ruta+"#")
+            if os.path.exists( ruta ):
+                logger.info("[library_service.py] Actualizando "+serie[0])
+                item = Item(url=serie[1], show=serie[0])
+                try:
+                    if serie[2].strip()=='seriesyonkis': itemlist = seriesyonkis.episodios(item)
+                    if serie[2].strip()=='cuevana': itemlist = cuevana.episodios(item)
+                except:
+                    itemlist = []
+            else:
+                logger.info("[library_service.py] No actualiza "+serie[0]+" (no existe el directorio)")
+                itemlist=[]
+                
+            i=0
+            for item in itemlist:
+                i = i + 1
+                item.show=serie[0].strip()
+                if i<len(itemlist):
+                    library.savelibrary( titulo=item.title , url=item.url , thumbnail=item.thumbnail , server=item.server , plot=item.plot , canal=item.channel , category="Series" , Serie=item.show , verbose=False, accion="play_from_library", pedirnombre=False, subtitle=item.subtitle )
+        
+        import xbmc
+        xbmc.executebuiltin('UpdateLibrary(video)')
+    else:
+        logger.info("No actualiza la biblioteca, está desactivado en la configuración de pelisalacarta")
 
 except:
     logger.info("[library_service.py] No hay series para actualizar")
