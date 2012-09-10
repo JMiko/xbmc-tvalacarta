a1670ac5ef722ad70f18e4fcb1f5603fd0928908
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-09-10 23:57:58 +0000

    Añadidos patrones a hulkshare para que salgan los del canal tusnovelas

--- trunk/pelisalacarta/servers/hulkshare.py
+++ trunk/pelisalacarta/servers/hulkshare.py
@@ -22,7 +22,7 @@ def get_video_url( page_url , premium = False , user="" , password="", video_pas
     location = scrapertools.get_header_from_response(page_url, header_to_get="location")
     extension = scrapertools.get_filename_from_url(location)[-4:]
 
-    video_urls.append( [ extension + " [hulkshare]",location ] )
+    video_urls.append( [ "[hulkshare]",location ] )
     
     return video_urls
 
@@ -47,4 +47,19 @@ def find_videos(data):
         else:
             logger.info("  url duplicada="+url)
 
+    #http://www.tusnovelas.com/hl.php?v=5ju6iuif5e68
+    patronvideos  = 'tusnovelas.com/hl.php\?v\=([a-z0-9]+)'
+    logger.info("[hulkshare.py] find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+
+    for match in matches:
+        titulo = "[hulkshare]"
+        url = "http://www.hulkshare.com/dl/"+match
+        if url not in encontrados:
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'hulkshare' ] )
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada="+url)
+    
     return devuelve
