07d532587ad50a868a53f1bb4eaac00b17527524
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-09-24 23:26:48 +0000

    Mejorado patron uploaded

--- trunk/pelisalacarta/servers/uploadedto.py
+++ trunk/pelisalacarta/servers/uploadedto.py
@@ -115,4 +115,20 @@ def find_videos(data):
         else:
             logger.info("  url duplicada="+url)
 
+    #http://uploaded.net/file/dg0sqyse
+    patronvideos = 'http://uploaded.net/file/([a-zA-Z0-9]+)'
+    logger.info("[uploadedto.py] find_videos #"+patronvideos+"#")
+    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+
+    for match in matches:
+        titulo = "[uploaded.to]"
+        url = "http://uploaded.net/file/"+match
+        if url not in encontrados:
+            logger.info("  url="+url)
+            devuelve.append( [ titulo , url , 'uploadedto' ] )
+            encontrados.add(url)
+        else:
+            logger.info("  url duplicada="+url)
+
+
     return devuelve
