5c051d914636be63c6d091b9edf41be5122a3a9d
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2012-09-27 22:16:36 +0000

    Descargar de Aragón TV 1.0

---
+++ descargar/aragontv/core/config.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Gestión de parámetros de configuración
+#-------------------------------------------------------------------------------
+
+import os,re
+
+overrides = dict()
+
+def get_platform():
+    return ""
+
+def get_system_platform():
+    return ""
+    
+def open_settings():
+    return
+
+def get_setting(name):
+    if name in overrides:
+        dev = overrides[name]
+    elif name=="cache.mode":
+        dev = "2"
+    elif name=="cookies.dir":
+        dev = get_data_path()
+    else:
+        dev = ""
+    return dev
+    
+def set_setting(name,value):
+    overrides[name]=value
+
+def get_localized_string(code):
+    return ""
+
+def get_library_path():
+    return ""
+
+def get_temp_file(filename):
+    return os.path.join(get_data_path(),filename)
+
+def get_data_path():
+    data_path = os.path.join( os.path.expanduser("~") , ".descargar-telefe" )
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+    return data_path
+
+def get_runtime_path():
+    return os.getcwd()
---
+++ descargar/aragontv/servers/aragontv.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Conector para aragontv
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#------------------------------------------------------------
+
+import urlparse,urllib2,urllib,re
+import os
+
+from core import scrapertools
+from core import logger
+from core import config
+
+def get_video_url( page_url , premium = False , user="" , password="", video_password="", page_data="" ):
+    logger.info("[aragontv.py] get_video_url(page_url='%s')" % page_url)
+
+    # ANTES
+    #url:'mp4%3A%2F_archivos%2Fvideos%2Fweb%2F2910%2F2910.mp4',
+    #netConnectionUrl: 'rtmp%3A%2F%2Falacarta.aragontelevision.es%2Fvod'
+    #rtmp://iasoftvodfs.fplive.net/iasoftvod/web/980/980.mp4
+
+    # AHORA
+    #{ url:'mp4%3A%2Fweb%2F5573%2F5573.mp4', provider: 'rtmp' }
+    #netConnectionUrl: 'rtmp%3A%2F%2Faragontvvodfs.fplive.net%2Faragontvvod'
+    #rtmp://aragontvvodfs.fplive.net/aragontvvod/web/980/980.mp4
+    
+    itemlist = []
+
+    # Descarga la página
+    data = scrapertools.cache_page(page_url)
+    final = scrapertools.get_match(data,"url\:'(mp4\%3A[^']+)'")
+    principio = scrapertools.get_match(data,"netConnectionUrl\: '([^']+)'")
+
+    if urllib.unquote(principio).startswith("rtmp://aragon") or urllib.unquote(principio).startswith("rtmp://iasoft"):
+        url = principio+"/"+final[9:]
+    else:
+        url = principio+"/"+final
+    url = urllib.unquote(url)
+    logger.info("url="+url)
+
+    video_urls = []
+    video_urls.append( [ "RTMP [aragontv]" , url ] )
+
+    for video_url in video_urls:
+        logger.info("[aragontv.py] %s - %s" % (video_url[0],video_url[1]))
+
+    return video_urls
+
+# Encuentra vídeos del servidor en el texto pasado
+def find_videos(data):
+    encontrados = set()
+    devuelve = []
+
+    return devuelve
+
