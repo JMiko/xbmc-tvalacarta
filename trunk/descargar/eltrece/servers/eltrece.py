1589a37084d05fde5399b8d5078a6328c44ded4b
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2013-01-06 09:00:22 +0000

    Descargar de El Trece v1.1

--- trunk/descargar/eltrece/servers/eltrece.py
+++ trunk/descargar/eltrece/servers/eltrece.py
@@ -18,13 +18,31 @@ def get_video_url( page_url , premium = False , user="" , password="", video_pas
     #http://www.eltrecetv.com.ar/los-%C3%BAnicos/los-%C3%BAnicos-2012/00052062/cap%C3%ADtulo-28-los-%C3%BAnicos
     data = scrapertools.cache_page(page_url)
 
-    # Gracias a susi por la pista http://blog.tvalacarta.info/2011/02/11/como-descargar-videos-de-el-trece-tv-argentina/comment-page-3/#comment-46245
-    final_url = scrapertools.get_match(data,"data-video='13tv([^']+)'")
-    url = "http://ctv.eltrecetv.com.ar"+final_url
+    '''
+    <div id="video-b905578722aaa39b7d6481976f5af95c" class="video-eltrece"  data-levels='[{"bitrate":"600","file":"13tv\/2012\/12\/15\/Sos_Mi_H_14-12-12M500.mp4","width":"564"},{"bitrate":"1300","file":"13tv\/2012\/12\/15\/Sos_Mi_H_14-12-12M720.mp4","width":"564"}]' data-mobile='13tv/2012/12/15/Sos_Mi_H_14-12-12M240.mp4' data-autoplay="false" data-type="rtmp" data-image="http://cdn.eltrecetv.com.ar/sites/default/files/styles/564x317/public/showmatch_15_.jpg" width="564" height="317" data-width="564" data-height="317"><img typeof="foaf:Image" src="http://cdn.eltrecetv.com.ar/sites/default/files/styles/564x317/public/showmatch_15_.jpg" width="564" height="317" alt="" title="" /></div>
+    '''
+
+    video_urls = []
+
+    data_mobile = scrapertools.get_match(data,"data-mobile\='13tv/([^']+)'")
+    logger.info("data_mobile="+data_mobile)
+    url = "http://ctv.eltrecetv.com.ar/"+data_mobile
     extension = scrapertools.get_filename_from_url(url)[-4:]
     
-    video_urls = []
-    video_urls.append( [ extension+" [eltrece]" , url ] )
+    video_urls.append( [ extension+" (para móviles) [eltrece]" , url ] )
+
+    data_level = scrapertools.get_match(data,"data-levels\='(.*?)'")
+    data_level = data_level.replace("\\","")
+    logger.info("data_level="+data_level)
+    patron = '"bitrate"\:"(\d+)","file"\:"13tv/([^"]+)"'
+    matches = re.compile(patron,re.DOTALL).findall(data_level)
+
+    for bitrate,fileurl in matches:
+        logger.info("bitrate="+bitrate+", url="+fileurl)
+        url = "http://ctv.eltrecetv.com.ar/"+fileurl
+        extension = scrapertools.get_filename_from_url(url)[-4:]
+        
+        video_urls.append( [ extension+" ("+bitrate+" Kbps) [eltrece]" , url ] )
 
     for video_url in video_urls:
         logger.info("[eltrece.py] %s - %s" % (video_url[0],video_url[1]))
