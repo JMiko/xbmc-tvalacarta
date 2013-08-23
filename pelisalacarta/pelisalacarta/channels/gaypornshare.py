70e8b0a331b35beb1d230b2284bce5a01a2efa15
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2013-08-23 16:58:47 +0000

    Actualizado canal gaypornshare (gracias a sdfasd sdfa sdfas)

--- pelisalacarta/pelisalacarta/channels/gaypornshare.py
+++ pelisalacarta/pelisalacarta/channels/gaypornshare.py
@@ -35,7 +35,7 @@ def mainlist(item):
     logger.info("[gaypornshare.py] mainlist")
 
     itemlist = []
-    itemlist.append( Item(channel=__channel__, action="lista"  , title="Todas las Películas" , url="http://gaypornshare.org/category/gay/movies/",thumbnail="http://t1.pixhost.org/thumbs/3282/12031567_a152063_xlb.jpg"))    
+    itemlist.append( Item(channel=__channel__, action="lista"  , title="Todas las Películas" , url="http://gaypornshare.org/page/1/",thumbnail="http://t1.pixhost.org/thumbs/3282/12031567_a152063_xlb.jpg"))    
     itemlist.append( Item(channel=__channel__, title="Buscar"     , action="search") )
     return itemlist
 
@@ -56,7 +56,9 @@ def lista(item):
 
 
     # Extrae las entradas (carpetas)
-    patronvideos ='<div class="thumb">.*?<a href="([^"]+)".*?<img src="([^"]+)".*?alt="([^"]+)".*?</a>'
+ #<div class="post" id="post-xxx> <a href="http://gaypornshare.org/a-toy-story-2013/" title="A Toy Story (2013)"><img width="240" height="170" src="http://gaypornshare.org/wp-content/uploads/2013/07/18132279_a168223_xlb-300x213.jpg" class="attachment-240x180 wp-post-image" alt="A Toy Story (2013)" title="" /></a>
+                        
+    patronvideos ='<div class="post" id="post-.*?<a href="([^"]+)".*?<img.*?src="([^"]+)".*?alt="([^"]+)".*?</a>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match in matches:
@@ -75,19 +77,19 @@ def lista(item):
  
   
   # Extrae la marca de siguiente página
-
-    patronvideos ="<a href='([^']+)' class='nextpostslink'>([^']+)</a>"
+#<span class="current">3</span><a href='http://gaypornshare.org/page/4/' class="inactive">
+    patronvideos ="<span.*?current.*?</span><a href='([^']+)' class=\"inactive\">([^']+)</a>"
     matches2 = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match2 in matches2:
-        scrapedtitle = ">> siguiente"
+        scrapedtitle = ">> página "+match2[1]
         scrapedurl = match2[0]
         scrapedthumbnail = ""
         imagen = ""
         scrapedplot = match2[0]  
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
         itemlist.append( Item(channel=__channel__, action="lista", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
- 
+    itemlist.append( Item(channel=__channel__, action="mainlist", title="<< volver al inicio",  folder=True) )
  
     return itemlist
 
@@ -106,7 +108,9 @@ def search(item,texto):
 
     
     # Extrae las entradas (carpetas)
-    patronvideos ='<div class="thumb">.*?<a href="([^"]+)".*?<img src="([^"]+)".*?alt="([^"]+)".*?</a>'
+    #<div class="post" id="post-xxx><a href="http://gaypornshare.org/navigaytor-2007/" title="NaviGayTor (2007)"><img width="240" height="170" src="http://gaypornshare.org/wp-content/uploads/2013/07/18130228_a114299_xlb-300x213.jpg" class="attachment-240x180 wp-post-image" alt="NaviGayTor (2007)" title="" /></a>
+            
+    patronvideos ='<div class="post" id="post-.*?<a href="([^"]+)".*?<img.*?src="([^"]+)".*?alt="([^"]+)".*?</a>'
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match in matches:
@@ -125,21 +129,25 @@ def search(item,texto):
 
    
   # Extrae la marca de siguiente página
-    patronvideos ="<a href='([^']+)' class='nextpostslink'>([^']+)</a>"
+    patronvideos ="<span.*?current.*?</span><a href='([^']+)' class=\"inactive\">([^']+)</a>"
     matches2 = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match2 in matches2:
-        scrapedtitle = ">> página siguiente"
+        scrapedtitle = ">> página"+match2[1]
         scrapedurl = match2[0]
         scrapedthumbnail = ""
         imagen = ""
         scrapedplot = match2[0]  
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
         itemlist.append( Item(channel=__channel__, action="lista", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
- 
+    itemlist.append( Item(channel=__channel__, action="mainlist", title="<< volver al inicio",  folder=True) )
  
 
     return itemlist
+
+
+
+
 
 
 
@@ -149,29 +157,51 @@ def detail(item):
 
     # Descarga la pagina
     data = scrapertools.downloadpageGzip(item.url)
-    descripcion = ""
-    plot = ""
-    patrondescrip = 'SINOPSIS:(.*?)'
-    matches = re.compile(patrondescrip,re.DOTALL).findall(data)
-    if len(matches)>0:
-        try :
-            plot = unicode( descripcion, "utf-8" ).encode("iso-8859-1")
-        except:
-            plot = descripcion
+    
+    #busca los adf.ly
+    #<a href="http://adf.ly/S72Kc">TheFarm.avi 1.4 GB</a>
+    patron = '<a href="http.//adf.ly/([^"]+)">([^>]+)</a>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    
+    for match in matches:
+        titulo="[con adf.ly] "+match[1]
+        enlace="http://adf.ly/"+match[0]
+        itemlist.append( Item(channel=__channel__ , action="getadfly" ,  server="adfly",  title=titulo, url=enlace, thumbnail=item.thumbnail, plot=item.plot, folder=False))
 
-    # Busca los enlaces a los videos de : "Megavideo"
+
+    # Busca los enlaces a los videos de los servidores
     video_itemlist = servertools.find_video_items(data=data)
     for video_item in video_itemlist:
-        itemlist.append( Item(channel=__channel__ , action="play" , server=video_item.server, title=item.title+video_item.title,url=video_item.url, thumbnail=item.thumbnail, plot=item.plot, folder=False))
-
-    # Extrae los enlaces a los videos (Directo)
-    patronvideos = "file: '([^']+)'"
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    if len(matches)>0:
-        if not "www.youtube" in matches[0]:
-            itemlist.append( Item(channel=__channel__ , action="play" , server="Directo", title=item.title+" [directo]",url=matches[0], thumbnail=item.thumbnail, plot=item.plot))
+        itemlist.append( Item(channel=__channel__ , action="play" , server=video_item.server, title=item.title+video_item.title, url=video_item.url, thumbnail=item.thumbnail, plot=item.plot, folder=False))
 
     return itemlist
     
     
+
+ 
+def getadfly(item):
+    logger.info("[gaypornshare.py] play")
+    itemlist=[]
+
+    location = item.url
+    if item.server=="adfly":
+
+    
+        # Extrae la URL de saltar el anuncio en adf.ly
+        if location.startswith("http://adf"):
+            # Averigua el enlace
+            from servers import adfly
+            location = adfly.get_long_url(location)
+            logger.info("location="+location)
+
+        from servers import servertools
+        itemlist=servertools.find_video_items(data=location)
+        for videoitem in itemlist:
+            videoitem.channel=__channel__
+            videoitem.folder=False
+
+    else:
+        itemlist.append(item)
+
+    return itemlist
 
