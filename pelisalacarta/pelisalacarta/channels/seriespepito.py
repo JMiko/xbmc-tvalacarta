9a3bbcf89db57f40e58a41d404bdebcf2469597f
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2014-05-29 22:00:50 +0000

    Arreglado el problema para ver el vídeo en los pepitos

--- pelisalacarta/pelisalacarta/channels/seriespepito.py
+++ pelisalacarta/pelisalacarta/channels/seriespepito.py
@@ -5,6 +5,8 @@
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 #------------------------------------------------------------
 import urlparse,urllib2,urllib,re
+import os, sys
+import hashlib
 
 from core import logger
 from core import config
@@ -20,12 +22,16 @@ __language__ = "ES"
 
 DEBUG = config.get_setting("debug")
 
-SERIESPEPITO_REQUEST_HEADERS = []
-SERIESPEPITO_REQUEST_HEADERS.append(["User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:28.0) Gecko/20100101 Firefox/28.0"])
-SERIESPEPITO_REQUEST_HEADERS.append(["Accept-Encoding","gzip, deflate"])
-SERIESPEPITO_REQUEST_HEADERS.append(["Accept-Language","es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3"])
-SERIESPEPITO_REQUEST_HEADERS.append(["Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"])
-SERIESPEPITO_REQUEST_HEADERS.append(["Connection","keep-alive"])
+ENLACESPEPITO_REQUEST_HEADERS = [
+    ["User-Agent" , "Mozilla/5.0 (Windows NT 6.1; rv:28.0) Gecko/20100101 Firefox/28.0"],
+    ["Accept-Encoding","gzip, deflate"],
+    ["Accept-Language" , "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3"],
+    ["Accept" , "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"],
+    ["Connection" , "keep-alive"]
+]
+
+SERIES_PEPITO    = 0
+PELICULAS_PEPITO = 1
 
 def isGeneric():
     return True
@@ -309,16 +315,7 @@ def play(item):
     logger.info("[seriespepito.py] play")
     itemlist=[]
     
-    # Lee la página
-    data = scrapertools.cache_page(item.url, headers = SERIESPEPITO_REQUEST_HEADERS)
-    
-    # La utiliza como referer del enlace real
-    SERIESPEPITO_REQUEST_HEADERS.append(["Referer",item.url])
-
-    # Descarga el enlace real
-    item.url = scrapertools.find_single_match(data,'href="(http.//www.enlacespepito.com/[^"]+)">')
-    mediaurl = scrapertools.get_header_from_response(item.url, header_to_get="location", headers = SERIESPEPITO_REQUEST_HEADERS)
-
+    mediaurl = get_server_link_series(item.url)
 
     # Busca el vídeo
     videoitemlist = servertools.find_video_items(data=mediaurl)
@@ -333,6 +330,71 @@ def play(item):
             i=i+1
 
     return itemlist
+
+def get_cookie(html):
+    import cookielib
+
+    ficherocookies = os.path.join( config.get_setting("cookies.dir"), 'cookies.dat' )
+    cj = cookielib.MozillaCookieJar()
+    cj.load(ficherocookies,ignore_discard=True)
+
+    cookie_pat = "cookie\('([a-zA-Z0-9]+)'\);"
+    cookie_name = scrapertools.find_single_match(html, cookie_pat)
+
+    cookie_value = ""
+
+    for cookie in cj:
+        if cookie.name == cookie_name:
+            cookie_value = cookie.value
+            break
+
+    return cookie_value
+
+# Busca el enlace correcto y lo procesa capturando los caracteres
+# y posiciones del Javascript
+#
+def convert_link(html, link_type):
+    hash_seed = get_cookie(html);
+
+    hash = hashlib.sha256(hash_seed).hexdigest()
+    if link_type == PELICULAS_PEPITO:
+        hash += '0'
+
+    HREF_SEARCH_PAT = '<a class=".' + hash + '" target="_blank" href="http://www.enlacespepito.com\/([^\.]*).html"><i class="icon-(?:play|download)">'
+
+    href = list(scrapertools.find_single_match(html, HREF_SEARCH_PAT))
+
+    CHAR_REPLACE_PAT = '[a-z]\[(\d+)\]="(.)";'
+
+    matches = re.findall(CHAR_REPLACE_PAT , html, flags=re.DOTALL|re.IGNORECASE)
+    for match in matches:
+        href[int(match[0])] = match[1]
+
+    href = ''.join(href)
+
+    return 'http://www.enlacespepito.com/' + href + '.html'
+
+def get_server_link(first_link, link_type):
+
+    html = scrapertools.downloadpage(first_link, headers = ENLACESPEPITO_REQUEST_HEADERS)
+    fixed_link = convert_link(html, link_type)
+
+    # Sin el Referer da 404
+    ENLACESPEPITO_REQUEST_HEADERS.append(['Referer', first_link])
+
+    return scrapertools.get_header_from_response(fixed_link, header_to_get="location", headers = ENLACESPEPITO_REQUEST_HEADERS)
+
+# Estas funciones son las únicas que deberían llamarse desde fuera
+#
+def get_server_link_series(first_link):
+    return get_server_link(first_link, SERIES_PEPITO)
+
+def get_server_link_peliculas(first_link):
+    return get_server_link(first_link, PELICULAS_PEPITO)
+
+
+
+
 
 # Verificación automática de canales: Esta función debe devolver "True" si está ok el canal.
 def test():
