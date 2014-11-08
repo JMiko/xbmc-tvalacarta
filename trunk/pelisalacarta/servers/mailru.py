931621b2f5d0ba46de12f70e1317654a6034adba
tvalacarta@gmail.com <tvalacarta@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2014-11-08 20:26:47 +0000

    Actualizado patron mail.ru (gracias a robalo)

--- trunk/pelisalacarta/servers/mailru.py
+++ trunk/pelisalacarta/servers/mailru.py
@@ -25,16 +25,13 @@ def get_video_url( page_url , premium = False , user="" , password="", video_pas
     try:
         media_url_hd = scrapertools.get_match( data , '"videos":{[^,]+,"hd":"([^"]+)"' )
         media_url_hd += "|Cookie=video_key="+video_key
+        video_urls.append( [ scrapertools.get_filename_from_url(media_url_hd)[-4:] + " [mail.ru] HD",media_url_hd ] )
     except: pass
     try:
         media_url_sd = scrapertools.get_match( data , '"videos":{"sd":"([^"]+)"' )
         media_url_sd += "|Cookie=video_key="+video_key
+        video_urls.append( [ scrapertools.get_filename_from_url(media_url_sd)[-4:] + " [mail.ru] SD",media_url_sd ] )
     except: pass
-
-
-
-    video_urls.append( [ scrapertools.get_filename_from_url(media_url_hd)[-4:] + " [mail.ru] HD",media_url_hd ] )
-    video_urls.append( [ scrapertools.get_filename_from_url(media_url_sd)[-4:] + " [mail.ru] SD",media_url_sd ] )
 
     for video_url in video_urls:
         logger.info("[mail.ru] %s - %s" % (video_url[0],video_url[1]))
@@ -50,9 +47,12 @@ def find_videos(data):
     devuelve = []
 
     titulo = "[mail.ru]"
-    id_page_url = scrapertools.get_match(data,'/_myvideo/(\d+).html')
-    author_name = scrapertools.get_match(data,'/video/mail([^/]+)/')
-    url = "http://api.video.mail.ru/videos/mail/%s/_myvideo/%s.json" % (author_name,id_page_url)
+    if "http://api.video.mail.ru/videos/embed/" in data:
+        url = data.replace("embed/","").replace(".html",".json")
+    else:
+        id_page_url = scrapertools.get_match(data,'/_myvideo/(\d+).html')
+        author_name = scrapertools.get_match(data,'/video/mail([^/]+)/')
+        url = "http://api.video.mail.ru/videos/mail/%s/_myvideo/%s.json" % (author_name,id_page_url)
 
     if url not in encontrados:
         logger.info("  url=%s" % (url))
