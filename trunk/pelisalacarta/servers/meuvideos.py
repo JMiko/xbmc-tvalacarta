91c05eab75ceda269fc73a743dae0e5f84c7b112
divadr@gmail.com <divadr@gmail.com@8cfa1fc8-e1b1-11dd-ab07-b19b4dad1b3d>
2015-02-17 18:57:57 +0000

    Añadida funcion para comprobar si el video existe en meuvideos.py

--- trunk/pelisalacarta/servers/meuvideos.py
+++ trunk/pelisalacarta/servers/meuvideos.py
@@ -17,10 +17,46 @@ import time
 
 def test_video_exists( page_url ):
     logger.info("[meuvideos] test_video_exists(page_url='%s')" % page_url)
+    data = SaltarPubli(page_url)
+    patron ='<div id="over_player_msg">([^"]+)<span id='
+    logger.info(data)
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    if len(matches) >0:
+      Estado = matches[0].replace("<br>", "<br/>") 
+      patron = "jah\('(.*?)'\,"
+      matches = re.compile(patron,re.DOTALL).findall(data)
+      data = scrapertools.downloadpage(matches[0])
+      Estado = Estado + " "+ scrapertools.get_match(data,"html\('(.*?)'\)")
+      logger.info(Estado)
+      return False, Estado
+ 
     return True,""
 
 def get_video_url( page_url , premium = False , user="" , password="", video_password="" ):
     logger.info("[meuvideos.py] url="+page_url)
+    data = SaltarPubli(page_url)
+    data = scrapertools.get_match(data,"return p}(.*?)'\)\)\)")
+    patron ='\(\'(.*?)\'\,([0-9]+)\,([0-9]+)\,\'(.*?)\'\.'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    for p, a, c, k in matches:
+      c=int(c)
+      a = int(a)
+      while(c>0):
+        c=c-1
+        if k.split("|")[c]:
+          patron = ur'\b'+int2base(c,a)+ur'\b'
+          p = re.sub(patron, k.split("|")[c], p, re.UNICODE)    
+      patron='setup\({file:"([^"]+)",'
+      matches = re.compile(patron,re.DOTALL).findall(p)
+      media_url=matches[0]
+
+    
+    video_urls = []
+    video_urls.append( [ scrapertools.get_filename_from_url(media_url)[-4:]+" [meuvideos]",media_url])
+
+    return video_urls
+    
+def SaltarPubli(page_url):
     data = scrapertools.cache_page( page_url )
     patron = '<Form method="POST">.{1}<input type="hidden" name="op" value="([^"]*)">.{1}<input type="hidden" name="usr_login" value="([^"]*)">.{1}<input type="hidden" name="id" value="([^"]*)">.{1}<input type="hidden" name="fname" value="([^"]*)">.{1}<input type="hidden" name="referer" value="([^"]*)">.{1}<input type="hidden" name="hash" value="([^"]*)">'
     matches = re.compile(patron,re.DOTALL).findall(data)
@@ -30,26 +66,7 @@ def get_video_url( page_url , premium = False , user="" , password="", video_pas
       post=urllib.urlencode({"op":op,"usr_login":usr_login,"id":id,"fname":fname, "referer":referer,"hash":hash,"imhuman":"Proceed to video"})
       url = page_url
       data = scrapertools.cache_page(url,post=post)
-      data = scrapertools.get_match(data,"return p}(.*?)'\)\)\)")
-      patron ='\(\'(.*?)\'\,([0-9]+)\,([0-9]+)\,\'(.*?)\'\.'
-      matches = re.compile(patron,re.DOTALL).findall(data)
-      for p, a, c, k in matches:
-        c=int(c)
-        a = int(a)
-        while(c>0):
-          c=c-1
-          if k.split("|")[c]:
-            patron = ur'\b'+int2base(c,a)+ur'\b'
-            p = re.sub(patron, k.split("|")[c], p, re.UNICODE)    
-        patron='setup\({file:"([^"]+)",'
-        matches = re.compile(patron,re.DOTALL).findall(p)
-        media_url=matches[0]
-
-    
-    video_urls = []
-    video_urls.append( [ scrapertools.get_filename_from_url(media_url)[-4:]+" [meuvideos]",media_url])
-
-    return video_urls
+    return data
     
 def int2base(integer, base):
         if not integer: return '0'
